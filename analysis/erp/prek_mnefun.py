6a084282e1bedba886f734aa8c46bf7eb7973ff2
nordme <nordme@uw.edu>
2019-11-04 11:25:06 -0800

    Changes that allow for easier control of the scripts during reruns

--- analysis/erp/prek_mnefun.py
+++ analysis/erp/prek_mnefun.py
@@ -25,26 +25,43 @@ import numpy as np
 import mnefun
 from prek_score import prek_score
 
-params = mnefun.Params(tmin=-0.1, tmax=1, t_adjust=-0.067, n_jobs=18,
-                       proj_sfreq=200, n_jobs_fir='cuda',
+pre_or_post = 'post' # convenience variable for rerunning 
+target_dir = '/mnt/scratch/prek/%s_camp/twa_hp/erp/' % pre_or_post
+
+params = mnefun.Params(tmin=-0.1, tmax=1, t_adjust=-0.067, n_jobs=2,
+                       proj_sfreq=200, n_jobs_fir=2,
                        filter_length='5s', lp_cut=80.,
-                       n_jobs_resample='cuda',
+                       n_jobs_resample=2,
                        bmin=-0.1, bem_type='5120')
 # load subjects
-with open(os.path.join('..', '..', 'params', 'subjects.yaml'), 'r') as f:
-    params.subjects = yaml.load(f, Loader=yaml.FullLoader)
+# with open(os.path.join('..', '..', 'params', 'subjects.yaml'), 'r') as f:
+#     params.subjects = yaml.load(f, Loader=yaml.FullLoader)
 
+import os.path as op
+erp_dir = '/mnt/scratch/prek/%s_camp/twa_hp/erp/' % (pre_or_post)
+skip = ['prek_1259', 'prek_1451']
+subjects = [x for x in os.listdir(erp_dir) if op.isdir(op.join(erp_dir, x)) and 'prek' in x and not np.in1d(x, skip)]
+subjects.sort()
+print(subjects)
+structurals = ['PREK%s' %x[4:] for x in subjects]
+
+params.subjects = subjects
+
+params.work_dir = target_dir
+params.subjects_dir = '/mnt/scratch/prek/anat'
 params.score = prek_score
-params.structurals = params.subjects
+params.structurals = structurals
 params.dates = [(2013, 0, 00)] * len(params.subjects)
 # define which subjects to run
 # params.subject_indices = [0]
-params.subject_indices = list(range(len(params.subjects)))
+params.subject_indices = np.arange(len(params.subjects))
+# params.subject_indices = np.arange(0,46)
+# params.subject_indices = [47]
 # Aquistion params
-params.acq_ssh = 'maggie@kasga.ilabs.uw.edu'
+params.acq_ssh = 'nordme@kasga.ilabs.uw.edu'
 params.acq_dir = '/brainstudio/prek/'
-params.sws_ssh = 'maggie@kasga.ilabs.uw.edu'
-params.sws_dir = '/data07/maggie/prek/'
+params.sws_ssh = 'nordme@kasga.ilabs.uw.edu'
+params.sws_dir = '/data07/nordme/prek/'
 # SSS options
 params.sss_type = 'python'
 params.sss_regularize = 'in'
@@ -64,10 +81,11 @@ params.flat = dict(grad=1e-13, mag=1e-15)
 params.auto_bad_flat = None
 params.auto_bad_meg_thresh = 10
 # naming
-params.run_names = ['%s_erp_pre']
+params.run_names = ['%s_erp_'+ pre_or_post]
+params.subject_run_indices = None
 params.get_projs_from = np.arange(1)
 params.inv_names = ['%s']
-params.inv_runs = [np.arange(1)]
+params.inv_runs = np.arange(1)
 params.runs_empty = []
 # proj
 params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
@@ -76,7 +94,9 @@ params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
 params.cov_method = 'empirical'
 params.bem_type = '5120'
 params.compute_rank = True
+params.cov_rank = None
 # Epoching
+params.reject_epochs_by_annot = False   # new param due to EOG annots
 params.in_names = ['words', 'faces', 'cars', 'aliens']
 params.in_numbers = [10, 20, 30, 40]
 params.analyses = ['All',
@@ -122,6 +142,16 @@ params.report_params.update(  # add plots
         dict(analysis='Conditions', name='aliens',
              inv='%s-80-sss-meg-free-inv.fif')
     ],
+    whitening=[
+        dict(analysis='Conditions', name='words',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='faces',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='cars',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='aliens',
+             cov='%s-80-sss-cov.fif')
+    ],
     psd=False,
 )
 
@@ -129,13 +159,13 @@ mnefun.do_processing(
     params,
     fetch_raw=False,
     do_sss=False,        # do tSSS
-    do_score=True,       # do scoring
-    gen_ssp=True,        # generate ssps
-    apply_ssp=True,      # apply ssps
-    write_epochs=True,   # epoching & filtering
-    gen_covs=True,       # make covariance
-    gen_fwd=True,        # generate fwd model
-    gen_inv=True,        # general inverse
-    gen_report=False,    # print report
+    do_score=False,       # do scoring
+    gen_ssp=False,        # generate ssps
+    apply_ssp=False,      # apply ssps
+    write_epochs=False,   # epoching & filtering
+    gen_covs=False,       # make covariance
+    gen_fwd=False,        # generate fwd model
+    gen_inv=False,        # general inverse
+    gen_report=True,    # print report
     print_status=True    # show status
 )
