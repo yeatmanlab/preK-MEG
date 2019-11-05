8146d9f0e184987eb760e72d3148bfa57cfc73c8
Daniel McCloy <dan.mccloy@gmail.com>
2019-11-05 14:07:11 -0800

    do better

--- analysis/erp/aux_functions.py
+++ analysis/erp/aux_functions.py
@@ -31,12 +31,8 @@ def load_paths():
     return paths['data_root'], paths['subjects_dir'], paths['results_dir']
 
 
-def prep_cluster_stats_for_yaml(cluster_results):
+def prep_cluster_stats(cluster_results):
     (tvals, clusters, cluster_pvals, hzero) = cluster_results
-    # collect clustering results into dict. Hacky conversions to float,
-    # int, or list are because yaml doesn't understand numpy dtypes.
-    clusters = [[clust[0].astype(int).tolist(), clust[1].astype(int).tolist()]
-                for clust in clusters]
     stats = dict(n_clusters=len(clusters),
                  clusters=clusters,
                  tvals=tvals.tolist(),
