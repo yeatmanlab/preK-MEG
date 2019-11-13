11fdd94c6d94a0844f95595192925d7d0bd2eec0
Daniel McCloy <dan.mccloy@gmail.com>
2019-11-13 12:54:26 -0800

    do full pipeline with sLORETA too (#24)
    * do full pipeline with sLORETA too
    
    * better path variable & filename consistency
    
    * iterator -> list

--- analysis/erp/prek_contrast_prepost.py
+++ analysis/erp/prek_contrast_prepost.py
@@ -19,44 +19,45 @@ mne.cuda.init_cuda()
 
 # config paths
 _, _, results_dir = load_paths()
-avg_path = os.path.join(results_dir, 'group_averages')
+groupavg_path = os.path.join(results_dir, 'group_averages')
 mov_path = os.path.join(results_dir, 'movies')
-prepost_out_path = os.path.join(results_dir, 'prepost_contrasts')
-if not os.path.isdir(prepost_out_path):
-    os.makedirs(prepost_out_path, exist_ok=True)
+prepost_path = os.path.join(results_dir, 'prepost_contrasts')
+if not os.path.isdir(prepost_path):
+    os.makedirs(prepost_path, exist_ok=True)
 
 # config other
 conditions = ('words', 'faces', 'cars')  # we purposely omit 'aliens' here
-methods = ('dSPM',)  # dSPM, sLORETA, eLORETA
+methods = ('dSPM', 'sLORETA')  # dSPM, sLORETA, eLORETA
 
 # load params
 brain_plot_kwargs, movie_kwargs, subjects = load_params()
 
-# generate contrast pairs
-contrasts = combinations(conditions, 2)
+# generate contrast pairs (need a list so we can loop over it twice)
+contrasts = list(combinations(conditions, 2))
 
 # make contrast STCs & movies
 group = f'GrandAvgN{len(subjects)}FSAverage'
 # loop over algorithms
 for method in methods:
+    # dict to hold post-minus-pre for each condition
     condition_dict = dict()
     # load the STC for each condition, pre- and post-camp
     for cond in conditions:
         prepost_dict = dict()
         # subtract: post-camp minus pre-camp
         for prepost in ('pre', 'post'):
-            fname = f'{group}_{prepost}Camp_{method}_{cond}.stc'
-            fpath = os.path.join(avg_path, fname)
+            fname = f'{group}_{prepost}Camp_{method}_{cond}'
+            fpath = os.path.join(groupavg_path, fname)
             prepost_dict[prepost] = mne.read_source_estimate(fpath)
         stc = prepost_dict['post'] - prepost_dict['pre']
-        prepost_fname = f'{group}_PostCampMinusPreCamp_{method}_{cond}.stc'
-        stc.save(os.path.join(prepost_out_path, prepost_fname))
-        # make movie
-        brain = stc.plot(subject='fsaverage', **brain_plot_kwargs)
-        mov_fname = f'{prepost_fname[:-4]}.mov'
-        brain.save_movie(os.path.join(mov_path, mov_fname), **movie_kwargs)
+        prepost_fname = f'{group}_PostCampMinusPreCamp_{method}_{cond}'
+        stc.save(os.path.join(prepost_path, prepost_fname))
         # retain post-minus-pre for each condition, for condition contrasts
         condition_dict[cond] = stc
+        # make movie
+        brain = stc.plot(subject='fsaverage', **brain_plot_kwargs)
+        mov_fname = f'{prepost_fname}.mov'
+        brain.save_movie(os.path.join(mov_path, mov_fname), **movie_kwargs)
     # clean up
     del prepost_dict, brain
 
@@ -64,9 +65,9 @@ for method in methods:
     for (cond_0, cond_1) in contrasts:
         stc = condition_dict[cond_0] - condition_dict[cond_1]
         contr = f'{cond_0.capitalize()}Minus{cond_1.capitalize()}'
-        contr_fname = f'{group}_PostCampMinusPreCamp_{method}_{contr}.stc'
-        stc.save(os.path.join(prepost_out_path, contr_fname))
+        contr_fname = f'{group}_PostCampMinusPreCamp_{method}_{contr}'
+        stc.save(os.path.join(prepost_path, contr_fname))
         # make movie
         brain = stc.plot(subject='fsaverage', **brain_plot_kwargs)
-        mov_fname = f'{contr_fname[:-4]}.mov'
+        mov_fname = f'{contr_fname}.mov'
         brain.save_movie(os.path.join(mov_path, mov_fname), **movie_kwargs)
