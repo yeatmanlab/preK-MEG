7560b4bc0e20e315fa2d2f2be85b5d95fb070803
Daniel McCloy <dan.mccloy@gmail.com>
2019-11-22 14:32:29 -0800

    fix cluster movie output dir

--- analysis/erp/prek_make_cluster_movie_from_stills.bash
+++ analysis/erp/prek_make_cluster_movie_from_stills.bash
@@ -3,10 +3,13 @@ data_root="/mnt/scratch/prek"
 cluster_root="$data_root/results/clustering"
 read -r cluster_dir<"$cluster_root/most-recent-clustering.txt"
 frames_dir="$cluster_dir/frames"
-movies_dir="$data_root/results/movies/clustering"
+movies_subdir=$(echo "$cluster_dir" | rev | cut -d / -f 2 | rev)
+movies_dir="$data_root/results/movies/clustering/$movies_subdir"
+mkdir -p $movies_dir
 for dir in $(ls $frames_dir); do
-    ffmpeg -framerate 5 \
-        -i ${dir%frames}%03d.png \
+    ffmpeg -y \
+        -framerate 6 \
+        -i "$frames_dir/$dir/${dir%frames}%03d.png" \
         -c:v libx264 \
         -pix_fmt yuv420p \
         "$movies_dir/${dir%_frames}.mp4"
