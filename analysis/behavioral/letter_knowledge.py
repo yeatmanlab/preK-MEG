832e25c9c0f8f561d36f023d342611c9d4d36aac
Daniel McCloy <dan.mccloy@gmail.com>
2019-11-25 17:34:32 -0800

    use newer data source

--- analysis/behavioral/letter_knowledge.py
+++ analysis/behavioral/letter_knowledge.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
+"""
+Some exploratory plots for the behavioral data.
+"""
 
+
+import yaml
 from functools import partial
 import numpy as np
 from scipy.stats import zscore
@@ -7,29 +12,33 @@ import pandas as pd
 import seaborn as sns
 
 
-data = pd.read_csv('LetterKnowledge.csv')
+# load the data
+metadata = pd.read_csv('preK_InterventionData.csv')
 
-df = data.melt(id_vars='subID', value_vars=['UpperName', 'LowerName',
-                                            'UpperSound', 'LowerSound'])
-df.set_index('subID', inplace=True)
+# add info about cohort (upper/lower half) based on alphabet knowlege
+cohort_fname = 'letter_knowledge_cohorts.yaml'
+with open(os.path.join('..', '..', 'params', cohort_fname), 'r') as f:
+    cohort_groups = yaml.load(f, Loader=yaml.SafeLoader)
+mapping = {subj: key for key, value in cohort_groups.items() for subj in value}
+metadata['cohort'] = metadata['subID'].map(mapping)
 
-# raw scores
-g = sns.FacetGrid(df, col='variable')
-bins = np.arange(27)
-g.map(sns.distplot, 'value', bins=bins)
+# remove NaNs
+nan_rows = metadata[metadata.isna().any(axis=1)]
+metadata_nonans = metadata[np.logical_not(metadata.isna()).all(axis=1)]
 
-# z-scored
-dfz = df.copy()
-dfz['value'] = dfz.groupby('variable').transform(partial(zscore, ddof=1))
-g = sns.FacetGrid(dfz, col='variable')
-bins = np.linspace(-4, 4, 81)
-g.map(sns.distplot, 'value', bins=bins)
+# variable correlations
+value_vars = ['AlphabetKnowledge', 'Decoding', 'PhonemeMatching',
+              'PhonemeSegmenting', 'EVTRaw', 'Retell', 'Grammar']
+g = sns.pairplot(metadata_nonans, vars=value_vars, kind='reg', hue='cohort')
 
-# z-scored and averaged
-dfm = dfz.reset_index().groupby('subID')['value'].agg('mean')
-sns.distplot(dfm, bins=bins)
+# reshape & zscore
+mdf = metadata_nonans.melt(id_vars=['subID', 'visit'], value_vars=value_vars)
+mdf['zscore'] = mdf.groupby('variable')['value'].transform(partial(zscore,
+                                                                   ddof=1))
 
-# put it back together
-df = df.pivot(columns='variable', values='value')
-data.set_index('subID', inplace=True)
-data['newmean'] = dfm
+# variable distributions (z-scores)
+g = sns.FacetGrid(mdf, col='variable')
+bins = np.linspace(-5, 5, 101)
+g.map(sns.distplot, 'zscore', bins=bins)
+
+sns.lmplot('AlphabetKnowledge', 'PhonemeMatching', data=metadata)
