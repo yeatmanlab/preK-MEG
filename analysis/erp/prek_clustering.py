5e7c84023b69d9c0b1571806182fa0a813032fa4
Daniel McCloy <dan@mccloy.info>
2020-01-08 15:12:18 -0800

    refactor clustering to match do_contrasts

--- analysis/erp/prek_clustering.py
+++ analysis/erp/prek_clustering.py
@@ -25,40 +25,64 @@ n_jobs = 10
 threshold = None        # or dict(start=0, step=0.2) for TFCE
 spatial_exclude = True  # None -> whole brain; True -> use labels listed below
 
+
+def subset_X(X, hemi, conn_matrices, exclusion):
+    """Subset the cluster matrix, connectivity & exclusion label by hemi."""
+    this_X = (X[..., :hemi_nverts] if hemi == 'lh' else X[..., hemi_nverts:])
+    this_conn = conn_matrices[hemi]
+    this_excl = exclusion[hemi].vertices
+    results = dict(X=this_X, connectivity=this_conn, spatial_exclude=this_excl)
+    return results
+
+
+def cluster_each_hemi(X, conn_matrices, exclusion, group, timepoint, method,
+                      con):
+    for hemi in ('lh', 'rh'):
+        cluster_input = subset_X(X, hemi, conn_matrices, exclusion)
+        cluster_results = one_samp_test(**cluster_input)
+        stats = prep_cluster_stats(cluster_results)
+        # save clustering results
+        out_fname = f'{group}_{timepoint}_{method}_{con}_{hemi}.npz'
+        out_fpath = os.path.join(cluster_dir, out_fname)
+        np.savez(out_fpath, **stats)
+
+
 # load params
-brain_plot_kwargs, movie_kwargs, subjects = load_params()
+_, _, subjects = load_params()
 
 # config paths
 data_root, subjects_dir, results_dir = load_paths()
+groupavg_path = os.path.join(results_dir, 'group_averages')
 
 # set cache dir
 cache_dir = os.path.join(data_root, 'cache')
-if not os.path.isdir(cache_dir):
-    os.mkdir(cache_dir)
+os.makedirs(cache_dir, exist_ok=True)
 mne.set_cache_dir(cache_dir)
 
 # config other
-conditions = ('words', 'faces', 'cars')  # we purposely omit 'aliens' here
 methods = ('dSPM', 'sLORETA')  # dSPM, sLORETA, eLORETA
+timepoints = ('preCamp', 'postCamp')
+conditions = ['words', 'faces', 'cars']  # we purposely omit 'aliens' here
+contrasts = {f'{contr[0].capitalize()}Minus{contr[1].capitalize()}': contr
+             for contr in list(combinations(conditions, 2))}
 
 # load cohort info (keys Language/LetterIntervention and Lower/UpperKnowledge)
 intervention_group, letter_knowledge_group = load_cohorts()
 
-# generate contrast pairs (need a list so we can loop over it twice)
-contrasts = list(combinations(conditions, 2))
-
 # assemble groups to iterate over
-# groups = dict(GrandAvg=subjects)
-# groups.update(letter_knowledge_group)
-groups = dict()
+groups = dict(GrandAvg=subjects)
 groups.update(intervention_group)
+groups.update(letter_knowledge_group)
 
 # load fsaverage source space to get connectivity matrix
 fsaverage_src_path = os.path.join(subjects_dir, 'fsaverage', 'bem',
                                   'fsaverage-ico-5-src.fif')
 fsaverage_src = mne.read_source_spaces(fsaverage_src_path)
 hemi_nverts = len(fsaverage_src[0]['vertno'])
-conn_matrix = mne.spatial_src_connectivity(fsaverage_src)
+
+conn_matrices = dict(both=mne.spatial_src_connectivity(fsaverage_src),
+                     lh=mne.spatial_src_connectivity(fsaverage_src[0]),
+                     rh=mne.spatial_src_connectivity(fsaverage_src[1]))
 
 if spatial_exclude is not None:
     # labels to exclude, see 10.1016/j.neuroimage.2010.06.010
@@ -93,8 +117,13 @@ if spatial_exclude is not None:
         exclusion[hemi].vertices = \
             np.intersect1d(exclusion[hemi].vertices,
                            fsaverage_src[hemi_idx]['vertno'])
-    spatial_exclude = np.concatenate([exclusion['lh'].vertices,
-                                      exclusion['rh'].vertices + hemi_nverts])
+        if hemi == 'rh':
+            exclusion[hemi].vertices += hemi_nverts
+    name = ' + '.join(exclusion[hemi].name for hemi in ('lh', 'rh'))
+    exclusion['both'] = mne.BiHemiLabel(exclusion['lh'], exclusion['rh'],
+                                        name=name)
+    exclusion['both'].vertices = np.concatenate([exclusion['lh'].vertices,
+                                                 exclusion['rh'].vertices])
 # cluster results get different subfolders depending on threshold / exclude
 cluster_root = os.path.join(results_dir, 'clustering')
 if spatial_exclude is not None:
@@ -116,83 +145,90 @@ os.makedirs(cluster_dir, exist_ok=True)
 one_samp_test = partial(spatio_temporal_cluster_1samp_test,
                         threshold=threshold,
                         n_permutations=1024,
-                        connectivity=conn_matrix,
                         n_jobs=n_jobs,
                         seed=rng,
-                        spatial_exclude=spatial_exclude,
                         buffer_size=1024)
 
-# loop over algorithms
+# loop over source localization algorithms
 for method in methods:
-    condition_dict = dict()
+    data_dict = dict()
     # loop over groups
     for group_name, group_members in groups.items():
         group = f'{group_name}N{len(group_members)}FSAverage'
-        condition_dict[group] = dict()
+        data_dict[group] = dict()
         # loop over pre/post measurement time
-        for prepost in ('pre', 'post'):
-            condition_dict[group][prepost] = dict()
+        for timepoint in timepoints:
+            # skip conditions we don't need / care about
+            if group_name.endswith('Knowledge') and timepoint == 'postCamp':
+                continue
+            data_dict[group][timepoint] = dict()
             # load the individual subject STCs for each condition
             for cond in conditions:
-                condition_dict[group][prepost][cond] = list()
+                data_dict[group][timepoint][cond] = list()
                 # loop over subjects
                 for s in group_members:
-                    this_subj = os.path.join(data_root, f'{prepost}_camp',
+                    this_subj = os.path.join(data_root,
+                                             f'{timepoint[:-4]}_camp',
                                              'twa_hp', 'erp', s, 'stc')
-                    fname = f'{s}FSAverage_{prepost}Camp_{method}_{cond}'
+                    fname = f'{s}FSAverage_{timepoint}_{method}_{cond}'
                     stc_path = os.path.join(this_subj, fname)
                     stc = mne.read_source_estimate(stc_path)
                     stc_data = stc.data.transpose(1, 0)  # noqa; need (subj, time, space)
-                    condition_dict[group][prepost][cond].append(stc_data)
-                condition_dict[group][prepost][cond] = \
-                    np.array(condition_dict[group][prepost][cond])
+                    data_dict[group][timepoint][cond].append(stc_data)
+                data_dict[group][timepoint][cond] = \
+                    np.array(data_dict[group][timepoint][cond])
 
-            # do the condition subtraction
-            #for (cond_0, cond_1) in contrasts:
-            #    X = (condition_dict[group][prepost][cond_0] -
-            #         condition_dict[group][prepost][cond_1])
-            #    # do each hemisphere separately:
-            #    for hemi in ('lh', 'rh'):
-            #        this_X = (X[..., :hemi_nverts] if hemi == 'lh' else
-            #                  X[..., hemi_nverts:])
-            #        cluster_results = one_samp_test(this_X)
-            #        stats = prep_cluster_stats(cluster_results)
-            #        # save clustering results
-            #        contr = f'{cond_0.capitalize()}Minus{cond_1.capitalize()}'
-            #        out_fname = f'{group}_{prepost}Camp_{method}_{contr}.npz'
-            #        out_fpath = os.path.join(cluster_dir, out_fname)
-            #        np.savez(out_fpath, **stats)
+            # CONTRAST TRIAL CONDITIONS
+            for con, (contr_0, contr_1) in contrasts.items():
+                X = (data_dict[group][timepoint][contr_0] -
+                     data_dict[group][timepoint][contr_1])
+                data_dict[group][timepoint][con] = X
+                cluster_each_hemi(X, conn_matrices, exclusion, group,
+                                  timepoint, method, con)
 
-        # do the post-pre subtraction for single conditions
-        for cond in conditions:
-            if group_name == 'LetterIntervention':
+        # CONTRAST POST-MINUS-PRE
+        timepoint = 'PostCampMinusPreCamp'
+        data_dict[group][timepoint] = dict()
+        for con in conditions + list(contrasts):
+            # skip conditions we don't need / care about
+            if group_name.endswith('Knowledge'):
                 continue
-            X = (condition_dict[group]['post'][cond] -
-                 condition_dict[group]['pre'][cond])
-            # do each hemisphere separately:
-            for hemi in ('lh', 'rh'):
-                this_X = (X[..., :hemi_nverts] if hemi == 'lh' else
-                          X[..., hemi_nverts:])
-                cluster_results = one_samp_test(this_X)
-                stats = prep_cluster_stats(cluster_results)
-                # save clustering results
-                out_fname = f'{group}_PostCampMinusPreCamp_{method}_{cond}_{hemi}.npz'  # noqa
-                out_fpath = os.path.join(cluster_dir, out_fname)
-                np.savez(out_fpath, **stats)
+            X = (data_dict[group]['postCamp'][con] -
+                 data_dict[group]['preCamp'][con])
+            data_dict[group][timepoint][con] = X
+            cluster_each_hemi(X, conn_matrices, exclusion, group,
+                              timepoint, method, con)
 
-        # do the post-pre subtraction for contrasts
-        for (cond_0, cond_1) in contrasts:
-            X = ((condition_dict[group]['post'][cond_0] -
-                  condition_dict[group]['post'][cond_1]) -
-                 (condition_dict[group]['pre'][cond_0] -
-                  condition_dict[group]['pre'][cond_1]))
-            for hemi in ('lh', 'rh'):
-                this_X = (X[..., :hemi_nverts] if hemi == 'lh' else
-                          X[..., hemi_nverts:])
-                cluster_results = one_samp_test(this_X)
-                stats = prep_cluster_stats(cluster_results)
-                # save clustering results
-                contr = f'{cond_0.capitalize()}Minus{cond_1.capitalize()}'
-                out_fname = f'{group}_PostCampMinusPreCamp_{method}_{contr}_{hemi}.npz'  # noqa
-                out_fpath = os.path.join(cluster_dir, out_fname)
-                np.savez(out_fpath, **stats)
+    # CONTRAST PRE-INTERVENTION LETTER KNOWLEDGE
+    timepoint = 'preCamp'
+    group_name = 'UpperMinusLowerKnowledge'
+    n_subj = {g: len(groups[g]) for g in letter_knowledge_group}
+    n = '-'.join([str(n_subj[g]) for g in letter_knowledge_group])
+    group = f'{group_name}N{n}FSAverage'
+    data_dict[group] = dict()
+    data_dict[group][timepoint] = dict()
+    keys = {g: f'{g}N{n_subj[g]}FSAverage' for g in letter_knowledge_group}
+    for con in conditions + list(contrasts):
+        X = (data_dict[keys['UpperKnowledge']][timepoint][con] -
+             data_dict[keys['LowerKnowledge']][timepoint][con])
+        data_dict[group][timepoint][con] = X
+        cluster_each_hemi(X, conn_matrices, exclusion, group,
+                          timepoint, method, con)
+
+    # CONTRAST EFFECT OF INTERVENTION ON COHORTS
+    # # skip this one because it doesn't make sense to do the subtraction when
+    # # dealing with individual subject data instead of averages.
+    # timepoint = 'PostCampMinusPreCamp'
+    # group_name = 'LetterMinusLanguageIntervention'
+    # n_subj = {g: len(groups[g]) for g in intervention_group}
+    # n = '-'.join([str(n_subj[g]) for g in intervention_group])
+    # group = f'{group_name}N{n}FSAverage'
+    # data_dict[group] = dict()
+    # data_dict[group][timepoint] = dict()
+    # keys = {g: f'{g}N{n_subj[g]}FSAverage' for g in intervention_group}
+    # for con in conditions + list(contrasts):
+    #     X = (data_dict[keys['LetterIntervention']][timepoint][con] -
+    #          data_dict[keys['LanguageIntervention']][timepoint][con])
+    #     data_dict[group][timepoint][con] = X
+    #     cluster_each_hemi(X, conn_matrices, exclusion, group,
+    #                       timepoint, method, con)
