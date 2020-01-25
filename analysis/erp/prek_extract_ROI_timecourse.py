ca07e07b4531a047d5695ad814c19b1299992333
Daniel McCloy <dan@mccloy.info>
2020-01-24 16:15:58 -0800

    attempted fix implementing subtractions

--- analysis/erp/prek_extract_ROI_timecourse.py
+++ analysis/erp/prek_extract_ROI_timecourse.py
@@ -8,6 +8,7 @@ Extract mean activation time course from region-of-interest.
 
 import os
 import re
+from itertools import combinations
 import numpy as np
 import pandas as pd
 from mayavi import mlab
@@ -29,6 +30,13 @@ with open(os.path.join(cluster_root, 'most-recent-clustering.txt'), 'r') as f:
     cluster_dir = f.readline().strip()
 ts_dir = os.path.join(cluster_dir, 'time-series')
 os.makedirs(ts_dir, exist_ok=True)
+
+# config other
+methods = ('dSPM', 'sLORETA')  # dSPM, sLORETA, eLORETA
+timepoints = ('preCamp', 'postCamp')
+conditions = ['words', 'faces', 'cars']  # we purposely omit 'aliens' here
+contrasts = {f'{contr[0].capitalize()}Minus{contr[1].capitalize()}': contr
+             for contr in list(combinations(conditions, 2))}
 
 # load cohort info (keys Language/LetterIntervention and Lower/UpperKnowledge)
 intervention_group, letter_knowledge_group = load_cohorts()
@@ -60,10 +68,16 @@ def extract_time_course(cluster_fname):
     avg_stc = mne.read_source_estimate(avg_stc_fpath)
 
     # extract condition names from filenames
-    group, timepoint, method, condition = avg_stc_fname.split('_')
-    group_key = group[:group.index('N')]
-    prepost = timepoint.rstrip('Camp')
-    hemi_str = cluster_fname.rstrip('.npz')[-2:]
+    grp, timept, method, condtn = avg_stc_fname.split('_')
+    group_key = grp[:grp.index('N')]
+    hemi_str = re.sub(r'\.npz$', '', cluster_fname)[-2:]
+
+    # convert condition names as needed
+    timepoints = dict(preCamp=['pre'], postCamp=['post'],
+                      PostCampMinusPreCamp=['post', 'pre'])[timept]
+    conditions = [x.lower() for x in condtn.split('Minus')]
+    groups = re.sub(r'(Intervention|Knowledge)$', '', group_key)
+    groups = [x.lower() for x in groups.split('Minus')]
 
     # loop over clusters
     for clu in signif_clu:
@@ -84,24 +98,44 @@ def extract_time_course(cluster_fname):
         verts = np.unique(avg_stc.vertices[hemi][spatial_idxs])
         label = mne.Label(verts, hemi=hemi_str, subject='fsaverage')
         label = label.restrict(fsaverage_src)
-        # loop over subjects
+
         time_courses = dict()
-        group_members = groups[group_key]
-        for s in group_members:
-            this_subj = os.path.join(data_root, f'{prepost}_camp',
-                                     'twa_hp', 'erp', s, 'stc')
-            fname = f'{s}FSAverage_{timepoint}_{method}_{condition}'
-            stc_path = os.path.join(this_subj, fname)
-            stc = mne.read_source_estimate(stc_path)
-            # extract label time course
-            time_course = mne.extract_label_time_course(stc, label,
-                                                        src=fsaverage_src,
-                                                        mode='pca_flip',
-                                                        allow_empty=True)
-            time_courses['s'] = time_course
-        df = pd.DataFrame(time_courses, index=stc.times)
+        # loop over conditions to handle subtractions
+        for timepoint in timepoints:
+            time_courses[timepoint] = dict()
+            for condition in conditions:
+                time_courses[timepoint][condition] = dict()
+                group_members = groups[group_key]
+                for s in group_members:
+                    this_subj = os.path.join(data_root, f'{timepoint}_camp',
+                                             'twa_hp', 'erp', s, 'stc')
+                    fname = f'{s}FSAverage_{timepoint}Camp_{method}_{condition}'  # noqa E501
+                    stc_path = os.path.join(this_subj, fname)
+                    stc = mne.read_source_estimate(stc_path)
+                    # extract label time course
+                    kwargs = dict(src=fsaverage_src, mode='pca_flip',
+                                  allow_empty=True)
+                    time_course = mne.extract_label_time_course(stc, label,
+                                                                **kwargs)
+                    time_courses[timepoint][condition][s] = time_course
+                # convert dict of each subj's time series to DataFrame
+                df = pd.DataFrame(time_courses[timepoint][condition],
+                                  index=stc.times)
+                time_courses[timepoint][condition] = df
+            # collapse contrasts
+            if len(conditions) > 1:
+                time_courses[timepoint] = (time_courses[timepoint][conditions[0]] -  # noqa E501
+                                           time_courses[timepoint][conditions[1]])   # noqa E501
+            else:
+                time_courses[timepoint] = time_courses[timepoint][condition]
+        if len(timepoints) > 1:
+            time_courses = (time_courses[timepoints[0]] -
+                            time_courses[timepoints[1]])
+        else:
+            time_courses = time_courses[timepoint]
+        # save DataFrame
         ts_fname = f'{avg_stc_fname}_cluster{clu:05}.csv'
-        df.to_csv(os.path.join(ts_dir, ts_fname))
+        time_courses.to_csv(os.path.join(ts_dir, ts_fname))
 
 
 cluster_fnames = sorted([x.name for x in os.scandir(cluster_dir)
