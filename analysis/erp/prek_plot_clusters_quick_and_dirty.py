9606a6382193137ce9278ca88bc5efef1eb44df4
Daniel McCloy <dan@mccloy.info>
2020-01-30 17:28:25 -0800

    WIP: plotting the timeseries alongside their clusters

--- analysis/erp/prek_plot_clusters_quick_and_dirty.py
+++ analysis/erp/prek_plot_clusters_quick_and_dirty.py
@@ -9,9 +9,10 @@ Plot movies with significant cluster regions highlighted.
 import os
 import re
 import numpy as np
+import pandas as pd
 from mayavi import mlab
 import mne
-from aux_functions import load_paths, load_params
+from aux_functions import load_paths, load_params, load_cohorts
 
 mlab.options.offscreen = True
 mne.cuda.init_cuda()
@@ -28,11 +29,155 @@ with open(os.path.join(cluster_root, 'most-recent-clustering.txt'), 'r') as f:
     cluster_dir = f.readline().strip()
 cluster_stc_dir = os.path.join(cluster_dir, 'stcs')
 img_dir = os.path.join(cluster_dir, 'images')
-for folder in (img_dir, cluster_stc_dir):
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
+timeseries_dir = os.path.join(cluster_dir, 'time-series')
+for folder in (img_dir, cluster_stc_dir, timeseries_dir):
+    os.makedirs(folder, exist_ok=True)
+
+# load cohort info (keys Language/LetterIntervention and Lower/UpperKnowledge)
+intervention_group, letter_knowledge_group = load_cohorts()
+
+# assemble groups to iterate over
+groups_dict = dict(GrandAvg=subjects)
+groups_dict.update(intervention_group)
+groups_dict.update(letter_knowledge_group)
+
+# load fsaverage source space
+fsaverage_src_path = os.path.join(subjects_dir, 'fsaverage', 'bem',
+                                  'fsaverage-ico-5-src.fif')
+fsaverage_src = mne.read_source_spaces(fsaverage_src_path)
+hemi_nverts = len(fsaverage_src[0]['vertno'])
 
 
+# helper function: reconstruct group dictionary key from filename
+def build_group_key(group):
+    if group in ('letter', 'language'):
+        key = f'{group.capitalize()}Intervention'
+    elif group in ('upper', 'lower'):
+        key = f'{group.capitalize()}Knowledge'
+    elif group == 'grandavg':
+        key = 'GrandAvg'
+    return key
+
+
+# helper function: plot the clusters as pseudo-STC
+def plot_clusters(stc, cluster_stc, signif_clu):
+    """Plot the clusters.
+
+    Each "time" in a cluster STC shows one significant cluster, except for time
+    zero, which shows the sum of all significant clusters. So here we plot each
+    "time" as a separate image (skipping time zero).
+    """
+    stc_tstep_ms = 1000 * stc.tstep  # in milliseconds
+    stc_dur_ms = 1000 * (stc.times[-1] - stc.times[0])
+    clim_dict = dict(kind='value', pos_lims=[0, stc_tstep_ms, stc_dur_ms])
+    for time_idx, this_time in enumerate(cluster_stc.times):
+        if time_idx == 0:
+            continue
+        brain = cluster_stc.plot(smoothing_steps='nearest',
+                                 clim=clim_dict,
+                                 time_unit='ms',
+                                 time_label='temporal extent',
+                                 initial_time=this_time,
+                                 **brain_plot_kwargs)
+        cluster_idx = signif_clu[time_idx - 1]
+        img_fname = cluster_fname.replace('.npz',
+                                          f'_cluster{cluster_idx:05}.png')
+        brain.save_image(os.path.join(img_dir, img_fname))
+
+
+# helper function: get timeseries in label for a particular subject
+def get_subj_time_course(subj, timept, method, con, label):
+    this_subj = os.path.join(data_root, f'{timept}_camp', 'twa_hp', 'erp',
+                             subj, 'stc')
+    fname = f'{subj}FSAverage_{timept}Camp_{method}_{con}'
+    stc_path = os.path.join(this_subj, fname)
+    stc = mne.read_source_estimate(stc_path)
+    # extract label time course
+    kwargs = dict(src=fsaverage_src, mode='pca_flip', allow_empty=True)
+    time_course = mne.extract_label_time_course(stc, label, **kwargs)
+    return np.squeeze(time_course)
+
+
+# helper function: assemble DataFrame of timeseries for each subj in each
+# condition pertinent to the current cluster result
+def extract_time_courses(avg_stc, cluster_fname, cluster_dict, cluster_idx):
+    """Extract the mean (across vertices) time series for each subject."""
+    # extract condition names from filenames
+    avg_stc_fname = re.sub(r'(_[lr]h)?\.npz$', '', cluster_fname)
+    group, timepoint, method, condition = avg_stc_fname.split('_')
+    hemi_str = re.sub(r'\.npz$', '', cluster_fname)[-2:]
+
+    # convert condition names as needed
+    timepoints = dict(preCamp=['pre'], postCamp=['post'],
+                      PostCampMinusPreCamp=['post', 'pre'])[timepoint]
+    conditions = [x.lower() for x in condition.split('Minus')]
+    groups = re.sub(r'(Intervention|Knowledge)$', '', group[:group.index('N')])
+    groups = [x.lower() for x in groups.split('Minus')]
+
+    # make sure we're right about which hemisphere the cluster is in
+    temporal_idxs, spatial_idxs = cluster_dict['clusters'][cluster_idx]
+    if all(spatial_idxs <= hemi_nverts):
+        assert hemi_str == 'lh'
+        hemi = 0
+    elif all(spatial_idxs >= hemi_nverts):
+        assert hemi_str == 'rh'
+        hemi = 1
+        spatial_idxs = spatial_idxs - hemi_nverts
+    else:
+        err = ("you seem to have a cluster that spans "
+               "hemispheres, this shouldn't happen.")
+        raise RuntimeError(err)
+
+    # select the vertices in the cluster & convert to Label
+    verts = np.unique(avg_stc.vertices[hemi][spatial_idxs])
+    label = mne.Label(verts, hemi=hemi_str, subject='fsaverage')
+    label = label.restrict(fsaverage_src)
+
+    time_courses = dict()
+    # loop over conditions to handle subtractions
+    for timept in timepoints:
+        time_courses[timept] = dict()
+        for con in conditions:
+            time_courses[timept][con] = dict()
+            for grp in groups:
+                time_courses[timept][con][grp] = dict()
+                group_key = build_group_key(grp)
+                group_members = groups_dict[group_key]
+                for s in group_members:
+                    tc = get_subj_time_course(s, timept, method, con, label)
+                    time_courses[timept][con][grp][s] = tc
+                # convert dict of each subj's time series to DataFrame
+                df = pd.DataFrame(time_courses[timept][con][grp],
+                                  index=range(len(avg_stc.times)))
+                df['time'] = avg_stc.times
+                df['group'] = grp
+                time_courses[timept][con][grp] = df
+            # combine DataFrames across groups
+            if len(groups) == 1:
+                time_courses[timept][con] = time_courses[timept][con][grp]
+            else:
+                dfs = (time_courses[timept][con][g] for g in groups)
+                time_courses[timept][con] = pd.concat(dfs)
+            time_courses[timept][con]['condition'] = con
+        # combine DataFrames across conditions
+        if len(conditions) == 1:
+            time_courses[timept] = time_courses[timept][con]
+        else:
+            dfs = (time_courses[timept][c] for c in conditions)
+            time_courses[timept] = pd.concat(dfs)
+        time_courses[timept]['timepoint'] = timept
+    # combine DataFrames across timepoints
+    if len(timepoints) == 1:
+        time_courses = time_courses[timept]
+    else:
+        dfs = (time_courses[t] for t in timepoints)
+        time_courses = pd.concat(dfs)
+    # clean up
+    time_courses.reset_index(inplace=True)
+    return time_courses
+
+
+# workhorse function
 def make_cluster_stc(cluster_fname):
     # load the STC
     stc_fname = re.sub(r'(_[lr]h)?\.npz$', '', cluster_fname)
@@ -63,29 +208,30 @@ def make_cluster_stc(cluster_fname):
                                                        tstep=stc_tstep_ms)
         has_signif_clusters = True
     except RuntimeError:
-        txt_fname = cluster_fname.replace('.npz', '_NO-SIGNIFICANT-CLUSTERS.txt')  # noqa
+        txt_fname = cluster_fname.replace('.npz', '_NO-SIGNIFICANT-CLUSTERS.txt')  # noqa E501
         txt_fpath = os.path.join(img_dir, txt_fname)
         with open(txt_fpath, 'w') as f:
             f.write('no significant clusters')
     if has_signif_clusters:
+        # save the quasi-STC
         cluster_stc.save(os.path.join(cluster_stc_dir, stc_fname))
+        # get indices for which clusters are significant
+        signif_clu = cluster_dict['good_cluster_idxs'][0]
         # plot the clusters
-        stc_dur_ms = 1000 * (stc.times[-1] - stc.times[0])
-        clim_dict = dict(kind='value', pos_lims=[0, stc_tstep_ms, stc_dur_ms])
-        # Each "time" in a cluster STC shows one signif. cluster, except for
-        # time zero, which shows the sum of all signif. clusters. So here we
-        # plot each "time" as a separate image (skipping the first time).
-        for idx, this_time in enumerate(cluster_stc.times):
-            if idx == 0:
-                continue
-            brain = cluster_stc.plot(smoothing_steps='nearest',
-                                     clim=clim_dict,
-                                     time_unit='ms',
-                                     time_label='temporal extent',
-                                     initial_time=this_time,
-                                     **brain_plot_kwargs)
-            img_fname = cluster_fname.replace('.npz', f'_cluster{idx:02}.png')
-            brain.save_image(os.path.join(img_dir, img_fname))
+        plot_clusters(stc, cluster_stc, signif_clu)
+        # for each significant cluster, extract the mean (across vertices) time
+        # series for each subject, save to a CSV, and plot alongside the
+        # cluster location.
+        for cluster_idx in signif_clu:
+            timeseries_dataframe = extract_time_courses(stc, cluster_fname,
+                                                        cluster_dict,
+                                                        cluster_idx)
+            # save to CSV
+            timeseries_fname = f'{stc_fname}_cluster{cluster_idx:05}.csv'
+            timeseries_dataframe.to_csv(os.path.join(timeseries_dir,
+                                                     timeseries_fname))
+            # TODO: plot time series alongside cluster location
+            # XXX XXX XXX TODO XXX XXX XXX
 
 
 cluster_fnames = sorted([x.name for x in os.scandir(cluster_dir)
