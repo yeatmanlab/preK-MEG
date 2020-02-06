a671952012fc2c93cdd508ec2c73be9eddcb53c1
nordme <38704848+nordme@users.noreply.github.com>
2020-02-06 15:57:49 -0800

    Changes that allow for easier control of the scripts during reruns (#21)
    * Changes that allow for easier control of the scripts during reruns
    
    * Return to yaml loading for subjects
    
    * use shrunk cov reg method with empty room
    
    * Minor refinements + enable cuda

--- analysis/erp/prek_mnefun.py
+++ analysis/erp/prek_mnefun.py
@@ -25,26 +25,37 @@ import numpy as np
 import mnefun
 from prek_score import prek_score
 
-params = mnefun.Params(tmin=-0.1, tmax=1, t_adjust=-0.067, n_jobs=18,
+pre_or_post = 'post' # convenience variable for rerunning 
+target_dir = '/mnt/scratch/prek/%s_camp/twa_hp/erp/' % pre_or_post
+
+params = mnefun.Params(tmin=-0.1, tmax=1, t_adjust=-0.067, n_jobs='cuda',
                        proj_sfreq=200, n_jobs_fir='cuda',
                        filter_length='5s', lp_cut=80.,
                        n_jobs_resample='cuda',
                        bmin=-0.1, bem_type='5120')
 # load subjects
 with open(os.path.join('..', '..', 'params', 'subjects.yaml'), 'r') as f:
-    params.subjects = yaml.load(f, Loader=yaml.FullLoader)
+    subjects = yaml.load(f, Loader=yaml.FullLoader)
 
+print(subjects)
+structurals = [x.upper() for x in subjects]
+
+params.subjects = subjects
+params.work_dir = target_dir
+params.subjects_dir = '/mnt/scratch/prek/anat'
 params.score = prek_score
-params.structurals = params.subjects
+params.structurals = structurals
 params.dates = [(2013, 0, 00)] * len(params.subjects)
 # define which subjects to run
 # params.subject_indices = [0]
-params.subject_indices = list(range(len(params.subjects)))
+params.subject_indices = np.arange(len(params.subjects))
+# params.subject_indices = np.arange(0,46)
+# params.subject_indices = [47]
 # Aquistion params
-params.acq_ssh = 'maggie@kasga.ilabs.uw.edu'
+params.acq_ssh = 'nordme@kasga.ilabs.uw.edu'
 params.acq_dir = '/brainstudio/prek/'
-params.sws_ssh = 'maggie@kasga.ilabs.uw.edu'
-params.sws_dir = '/data07/maggie/prek/'
+params.sws_ssh = 'nordme@kasga.ilabs.uw.edu'
+params.sws_dir = '/data07/nordme/prek/'
 # SSS options
 params.sss_type = 'python'
 params.sss_regularize = 'in'
@@ -64,19 +75,23 @@ params.flat = dict(grad=1e-13, mag=1e-15)
 params.auto_bad_flat = None
 params.auto_bad_meg_thresh = 10
 # naming
-params.run_names = ['%s_erp_pre']
+params.run_names = ['%s_erp_'+ pre_or_post]
+params.subject_run_indices = None
 params.get_projs_from = np.arange(1)
 params.inv_names = ['%s']
-params.inv_runs = [np.arange(1)]
-params.runs_empty = []
+params.inv_runs = np.arange(1)
+params.runs_empty = ['%s_erm']
 # proj
 params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
                     [1, 1, 0],  # EOG
                     [0, 0, 0]]  # Continuous (from ERM)
-params.cov_method = 'empirical'
+params.cov_method = 'shrunk'
 params.bem_type = '5120'
 params.compute_rank = True
+params.cov_rank = None
+params.force_erm_cov_rank_full=False
 # Epoching
+params.reject_epochs_by_annot = False   # new param due to EOG annots
 params.in_names = ['words', 'faces', 'cars', 'aliens']
 params.in_numbers = [10, 20, 30, 40]
 params.analyses = ['All',
@@ -122,13 +137,23 @@ params.report_params.update(  # add plots
         dict(analysis='Conditions', name='aliens',
              inv='%s-80-sss-meg-free-inv.fif')
     ],
+    whitening=[
+        dict(analysis='Conditions', name='words',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='faces',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='cars',
+             cov='%s-80-sss-cov.fif'),
+        dict(analysis='Conditions', name='aliens',
+             cov='%s-80-sss-cov.fif')
+    ],
     psd=False,
 )
 
 mnefun.do_processing(
     params,
     fetch_raw=False,
-    do_sss=False,        # do tSSS
+    do_sss=True,        # do tSSS
     do_score=True,       # do scoring
     gen_ssp=True,        # generate ssps
     apply_ssp=True,      # apply ssps
@@ -136,6 +161,6 @@ mnefun.do_processing(
     gen_covs=True,       # make covariance
     gen_fwd=True,        # generate fwd model
     gen_inv=True,        # general inverse
-    gen_report=False,    # print report
+    gen_report=True,    # print report
     print_status=True    # show status
 )
--- analysis/ssvef/prek_mnefun_ssvep.py
+++ analysis/ssvef/prek_mnefun_ssvep.py
@@ -14,40 +14,44 @@ Notes:
 
 """
 
-
-# pre_camp exceptions to run_names:
-# prek 1714: use pskt_02, pskt_03
-# prek_1936: use pskt_01, pskt_03
-# prek_1964: use pskt_01, pskt_03
-
+import os
+import os.path as op
 import mnefun
 import numpy as np
 
+print(mnefun)
 
-dir = '/home/nordme/data/prek/post_camp/fixed_hp/'
-skip = ['prek_1259', 'prek_1451', 'prek_1714', 'prek_1936', 'prek_1964']
-#subjects = [x for x in os.listdir(dir) if op.isdir(op.join(dir, x)) and 'prek' in x and not np.in1d(x, skip)]
-subjects = ['prek_1936', 'prek_1964']
-# subjects = ['prek_1714']
+fixed_or_twa = 'twa' # convenience variable for doing different runs
+pre_or_post = 'post'
 
-subjects.sort()
-print(subjects)
+if fixed_or_twa == 'fixed':
+    trans_to = (0., 0., 0.04)
+else:
+    trans_to = 'twa'
 
-params = mnefun.Params(tmin=-0.1, tmax=1, n_jobs=18,
-                       proj_sfreq=200, n_jobs_fir=18,
+pskt_dir = '/mnt/scratch/prek/%s_camp/%s_hp/pskt/' % (pre_or_post, fixed_or_twa)
+
+# load subjects
+with open(os.path.join('..', '..', 'params', 'subjects.yaml'), 'r') as f:
+    subjects = yaml.load(f, Loader=yaml.FullLoader)
+
+structurals = [x.upper() for x in subjects]
+
+params = mnefun.Params(tmin=-0.1, tmax=1, n_jobs='cuda',
+                       proj_sfreq=200, n_jobs_fir='cuda',
                        filter_length='5s', lp_cut=80., 
-                       n_jobs_resample=18,
+                       n_jobs_resample='cuda',
                        bmin=-0.1, bem_type='5120', )
-#1451 rename
 
 params.subjects = subjects
-params.work_dir = dir
-params.structurals = params.subjects
+params.work_dir = pskt_dir
+params.structurals = structurals
+params.subjects_dir = '/mnt/scratch/prek/anat'
 params.dates = [(2013, 0, 00)] * len(params.subjects)
 # define which subjects to run
 params.subject_indices = np.arange(len(params.subjects))
-# params.subject_indices = np.setdiff1d(np.arange(len(params.subjects)), np.arange(11))
-# params.subject_indices = [7]
+# params.subject_indices = np.arange(28,45)
+# params.subject_indices = [47]
 # Acquisition params
 params.acq_ssh = 'nordme@kasga.ilabs.uw.edu'
 params.acq_dir = '/brainstudio/prek/'
@@ -59,7 +63,7 @@ params.sss_regularize = 'in'
 params.tsss_dur = 4. # tSSS duration
 params.int_order = 8
 params.st_correlation = .98
-params.trans_to = (0., 0., 0.04) # time weighted average or fixed head position (change this to fixed pos for sensor analysis)
+params.trans_to = trans_to # time weighted average or fixed head position (change this to fixed pos for sensor analysis)
 params.coil_t_window = 'auto'
 params.movecomp='inter'
 # remove segments with < 3 good coils for at least 100 ms
@@ -72,9 +76,7 @@ params.flat = dict(grad=1e-13, mag=1e-15)
 params.auto_bad_flat = None
 params.auto_bad_meg_thresh = 10
 # naming
-# params.run_names = ['%s_pskt_02_pre', '%s_pskt_03_pre']
-params.run_names = ['%s_pskt_01_pre', '%s_pskt_03_pre']
-# params.run_names = ['%s_pskt_01_pre', '%s_pskt_02_pre']
+params.run_names = ['%s_pskt_01_' + pre_or_post, '%s_pskt_02_' + pre_or_post]
 params.get_projs_from = np.arange(2)
 params.inv_names = ['%s']
 params.inv_runs = [np.arange(1)]
@@ -86,56 +88,27 @@ params.proj_nums = [[1, 1, 0],  # ECG: grad/mag/eeg
 params.cov_method = 'empirical'
 params.bem_type = '5120'
 params.compute_rank = True
+params.cov_rank = None
 
 # No epoching for ssvep
+# No conditions for report
 
 params.report_params.update(  # add plots
     bem=True, 
-    sensor=[
-        dict(analysis='Conditions', name='words', times='peaks'),
-        dict(analysis='Conditions', name='faces', times='peaks'),
-        dict(analysis='Conditions', name='cars', times='peaks'),
-        dict(analysis='Conditions', name='aliens', times='peaks'),
-    ],
-    source=[
-        dict(analysis='Conditions', name='words',
-             inv='%s-80-sss-meg-free-inv.fif',
-             views=['lat', 'caudal'], size=(800, 800)), 
-        dict(analysis='Conditions', name='faces',
-             inv='%s-80-sss-meg-free-inv.fif', 
-             views=['lat', 'caudal'], size=(800, 800)),
-        dict(analysis='Conditions', name='cars',
-             inv='%s-80-sss-meg-free-inv.fif',
-             views=['lat', 'caudal'], size=(800, 800)),
-        dict(analysis='Conditions', name='aliens',
-             inv='%s-80-sss-meg-free-inv.fif',
-             views=['lat', 'caudal'], size=(800, 800)),
-    ],
-    snr=[
-        dict(analysis='Conditions', name='words',
-             inv='%s-80-sss-meg-free-inv.fif'),
-        dict(analysis='Conditions', name='faces',
-             inv='%s-80-sss-meg-free-inv.fif'),
-        dict(analysis='Conditions', name='cars',
-             inv='%s-80-sss-meg-free-inv.fif'),
-        dict(analysis='Conditions', name='aliens',
-             inv='%s-80-sss-meg-free-inv.fif')
-    ],
-    psd=False,
-)
-
+    psd=False
+)   
 
 mnefun.do_processing(
     params,
     fetch_raw=False,
-    do_sss=True, # do tSSS
+    do_sss=False, # do tSSS
     do_score=False,  # do scoring
-    gen_ssp=True, # generate ssps
-    apply_ssp=True, # apply ssps
+    gen_ssp=False, # generate ssps
+    apply_ssp=False, # apply ssps
     write_epochs=False, # epoching & filtering
     gen_covs=False, # make covariance 
     gen_fwd=False, # generate fwd model
     gen_inv=False, # general inverse
-    gen_report=False, #print report
+    gen_report=True, #print report
     print_status=True # show status
 )
