3ed64339bc16fb5c7acd554f9a45f40f4dbe027b
Daniel McCloy <dan@mccloy.info>
2020-02-21 15:20:24 -0800

    restore gray line plotting

--- analysis/erp/prek_plot_clusters_quick_and_dirty.py
+++ analysis/erp/prek_plot_clusters_quick_and_dirty.py
@@ -14,16 +14,20 @@ from matplotlib.image import imread
 import seaborn as sns
 from mayavi import mlab
 import mne
-from aux_functions import load_paths, load_params, get_dataframe_from_label
+from aux_functions import (load_paths, load_params, load_cohorts,
+                           get_dataframe_from_label)
 
 mlab.options.offscreen = True
 mne.cuda.init_cuda()
 n_jobs = 10
+
+# plot prep
 sns.set(style='whitegrid', font_scale=0.8)
+grey_vals = ['0.75', '0.55', '0.35']
+color_vals = ['#004488', '#bb5566', '#ddaa33']
 
 # load params
 brain_plot_kwargs, movie_kwargs, subjects = load_params()
-
 # config paths
 data_root, subjects_dir, results_dir = load_paths()
 cluster_root = os.path.join(results_dir, 'clustering')
@@ -35,6 +39,18 @@ img_dir = os.path.join(cluster_dir, 'images')
 timeseries_dir = os.path.join(cluster_dir, 'time-series')
 for folder in (img_dir, cluster_stc_dir, timeseries_dir):
     os.makedirs(folder, exist_ok=True)
+
+# load cohort info (keys Language/LetterIntervention and Lower/UpperKnowledge)
+intervention_group, letter_knowledge_group = load_cohorts()
+
+# assemble groups info
+groups_dict = dict(grandavg=subjects,
+                   language=intervention_group['LanguageIntervention'],
+                   letter=intervention_group['LetterIntervention'],
+                   lower=letter_knowledge_group['LowerKnowledge'],
+                   upper=letter_knowledge_group['UpperKnowledge'])
+for group, members in groups_dict.items():
+    groups_dict[group] = [f'prek_{n}' for n in members]
 
 title_dict = dict(language='Language Intervention cohort',
                   letter='Letter Intervention cohort',
@@ -92,7 +108,7 @@ def get_condition_names(cluster_fname):
 
 # helper function: assemble DataFrame of timeseries for each subj in each
 # condition pertinent to the current cluster result
-def extract_time_courses(avg_stc, cluster_fname, cluster_dict, cluster_idx):
+def get_label_from_cluster(avg_stc, cluster_fname, cluster_dict, cluster_idx):
     """Extract the mean (across vertices) time series for each subject."""
     (groups, timepoints, method, conditions, hemi_str, avg_stc_fname
      ) = get_condition_names(cluster_fname)
@@ -115,11 +131,7 @@ def extract_time_courses(avg_stc, cluster_fname, cluster_dict, cluster_idx):
     verts = np.unique(avg_stc.vertices[hemi][spatial_idxs])
     label = mne.Label(verts, hemi=hemi_str, subject='fsaverage')
     label = label.restrict(fsaverage_src)
-
-    # get the dataframe
-    time_courses = get_dataframe_from_label(label, fsaverage_src, [method],
-                                            timepoints, conditions)
-    return time_courses
+    return label
 
 
 # workhorse function
@@ -169,49 +181,56 @@ def make_cluster_stc(cluster_fname):
         # series for each subject, save to a CSV, and plot alongside the
         # cluster location.
         for cluster_idx in signif_clu:
-            # get time series and save it
-            df = extract_time_courses(stc, cluster_fname, cluster_dict,
-                                      cluster_idx)
-            timeseries_fname = f'{avg_stc_fname}_cluster{cluster_idx:05}.csv'
-            timeseries_fpath = os.path.join(timeseries_dir, timeseries_fname)
-            df.to_csv(timeseries_fpath, index=False)
-
-            # plot time series alongside cluster location
-            n_rows = len(groups) + 1
-            gridspec_kw = dict(height_ratios=[4] + [1] * (n_rows - 1))
-            fig, axs = plt.subplots(n_rows, 1, gridspec_kw=gridspec_kw,
-                                    figsize=(9, 13))
-            # draw the cluster brain image into first axes
+            # get label
+            label = get_label_from_cluster(stc, cluster_fname, cluster_dict,
+                                           cluster_idx)
+            # triage groups
+            all_interventions = ('letter', 'language')
+            all_pretest_cohorts = ('lower', 'upper')
+            if groups[0] in all_interventions:
+                all_groups = all_interventions
+            elif groups[0] in all_pretest_cohorts:
+                all_groups = all_pretest_cohorts
+            else:
+                all_groups = ['grandavg']
+            # set up figure
+            gridspec_kw = dict(height_ratios=[4] + [1] * len(all_groups))
+            fig, axs = plt.subplots(len(all_groups) + 1, 1,
+                                    gridspec_kw=gridspec_kw, figsize=(9, 13))
+            # draw the brain/cluster image into first axes
             img_fname = re.sub(r'\.npz$', f'_cluster{cluster_idx:05}.png',
                                cluster_fname)
             cluster_image = imread(os.path.join(img_dir, img_fname))
             axs[0].imshow(cluster_image)
             axs[0].set_axis_off()
             axs[0].set_title(cluster_fname)
+
             # prepare to plot
             all_conditions = ('words', 'faces', 'cars')
             all_timepoints = ('post', 'pre')
-            all_interventions = ('letter', 'language')
-            all_pretest_cohorts = ('lower', 'upper')
             plot_kwargs = dict(hue='condition', hue_order=all_conditions,
                                style='timepoint', style_order=all_timepoints)
-            grey_vals = ['0.75', '0.55', '0.35']
-            color_vals = ['#004488', '#bb5566', '#ddaa33']
-            colors = [color_vals[i] if c in conditions else grey_vals[i]
-                      for i, c in enumerate(all_conditions)]
-            # triage group definition
-            if groups[0] in all_interventions:
-                group_column = df['intervention']
-            elif groups[0] in all_pretest_cohorts:
-                group_column = df['pretest']
-            else:
-                group_column = np.full(df.shape[:1], 'grandavg')
-            # draw the timecourses
-            for group, ax in zip(groups, axs[1:]):
-                # draw the relevant lines in color
+            df = get_dataframe_from_label(label, fsaverage_src, [method],
+                                          all_timepoints, all_conditions)
+            # plot
+            for group, ax in zip(all_groups, axs[1:]):
+                # plot cluster-relevant lines in color, others gray (unless
+                # we're plotting the non-cluster-relevant group → all gray)
+                colors = [color_vals[i]
+                          if c in conditions and group in groups else
+                          grey_vals[i]
+                          for i, c in enumerate(all_conditions)]
+                # get just the data for this group
+                if group in all_interventions:
+                    group_column = df['intervention']
+                elif group in all_pretest_cohorts:
+                    group_column = df['pretest']
+                else:
+                    group_column = np.full(df.shape[:1], 'grandavg')
                 data = df.loc[(group_column == group) &
                               np.in1d(df['timepoint'], timepoints) &
-                              np.in1d(df['condition'], conditions)]
+                              np.in1d(df['condition'], all_conditions)]
+                # draw
                 with sns.color_palette(colors):
                     sns.lineplot(x='time', y='value', data=data, ax=ax,
                                  **plot_kwargs)
