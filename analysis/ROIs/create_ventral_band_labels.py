a30d2a70435fde9d5905b97b5f79f0924024e47d
Daniel McCloy <dan@mccloy.info>
2020-03-02 14:29:54 -0800

    combine ROI generation scripts

--- analysis/ROIs/create_ventral_band_labels.py
+++ analysis/ROIs/create_ventral_band_labels.py
@@ -17,8 +17,20 @@ with open(os.path.join('..', '..', 'params', 'paths.yaml'), 'r') as f:
 # make sure we have the parcellation
 mne.datasets.fetch_aparc_sub_parcellation(subjects_dir)
 
-# define which labels comprise each region
-reg = {1: ['lingual_2-lh'] + [f'lateraloccipital_{n}-lh' for n in (4, 5)],
+# load fsaverage source space
+fsaverage_src_path = os.path.join(subjects_dir, 'fsaverage', 'bem',
+                                  'fsaverage-ico-5-src.fif')
+fsaverage_src = mne.read_source_spaces(fsaverage_src_path)
+fsaverage_src = mne.add_source_space_distances(fsaverage_src, dist_limit=0)
+
+# define which labels comprise each region. Region 0 is a large label covering
+# most of ventral occipitotemporal cortex. Regions 1-5 are sub-bands within
+# region 0, starting with 1 at the occipital pole and moving anteriorly.
+reg = {0: [f'lateraloccipital_{n}-lh' for n in (6, 7, 8, 9, 10, 11)] +
+          [f'lingual_{n}-lh' for n in (1, 4, 8)] +
+          [f'fusiform_{n}-lh' for n in (1, 2, 3)] +
+          ['inferiortemporal_8-lh'],
+       1: ['lingual_2-lh'] + [f'lateraloccipital_{n}-lh' for n in (4, 5)],
        2: ['lingual_1-lh'] + [f'lateraloccipital_{n}-lh' for n in (3, 6, 11)],
        3: ['lingual_4-lh', 'fusiform_2-lh'] + [f'lateraloccipital_{n}-lh'
                                                for n in (7, 10)],
@@ -36,4 +48,6 @@ for region_number, label_names in reg.items():
     labels = mne.read_labels_from_annot(regexp=regexp, **label_kwargs)
     merged_label = sum(labels[1:], labels[0])
     merged_label.comment = ''
-    merged_label.save(f'ventral_band_{region_number}-lh.label')
+    merged_label = merged_label.restrict(fsaverage_src)
+    merged_label = merged_label.fill(fsaverage_src)
+    merged_label.save(f'ventral_ROI_{region_number}-lh.label')
