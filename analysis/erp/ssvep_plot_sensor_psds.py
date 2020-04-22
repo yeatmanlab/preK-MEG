2c8bf2495d04688440056d888ec5cd8a01ad66f9
Daniel McCloy <dan@mccloy.info>
2020-04-22 11:16:35 -0700

    fix n_axes

--- analysis/erp/ssvep_plot_sensor_psds.py
+++ analysis/erp/ssvep_plot_sensor_psds.py
@@ -47,7 +47,7 @@ for s in subjects:
         # plot topomap
         if plot_topo:
             bands = [(freq, f'{freq} Hz') for freq in (2, 4, 6, 12)]
-            fig, axs = plt.subplots(2, 5)
+            fig, axs = plt.subplots(2, len(bands))
             for row, ch_type in zip(axs, ('mag', 'grad')):
                 epochs.plot_psd_topomap(bands, bandwidth=bandwidth,
                                         ch_type=ch_type, vlim='joint',
