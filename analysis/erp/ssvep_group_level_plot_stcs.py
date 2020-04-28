ce97ed2f3f598b699506cb93f0df92e0600d0bbc
Daniel McCloy <dan@mccloy.info>
2020-04-28 11:05:06 -0700

    fix dict

--- analysis/erp/ssvep_group_level_plot_stcs.py
+++ analysis/erp/ssvep_group_level_plot_stcs.py
@@ -56,7 +56,7 @@ for timepoint in timepoints:
             fname = f'{group}-{timepoint}_camp-pskt{subdiv}-multitaper-{kind}'
             stcs[kind] = mne.read_source_estimate(os.path.join(in_dir, fname))
 
-        data = np.array([s.data for s in stcs])
+        data = np.array([stcs[kind].data for kind in stcs])
         lims = tuple(np.percentile(data, 0.95, 0.99, 0.999))
         clim = dict(kind='value', lims=lims)
         # plot it
