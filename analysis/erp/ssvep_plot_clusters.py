8f2b7e6b7075bcde2c4678cf4e881bbe5bb329d0
Daniel McCloy <dan@mccloy.info>
2020-05-27 20:09:16 -0700

    overhaul cluster plotting for TFCE

--- analysis/erp/ssvep_plot_clusters.py
+++ analysis/erp/ssvep_plot_clusters.py
@@ -42,7 +42,6 @@ all_freqs = stc.times
 vertices = stc.vertices
 stc_tstep_hz = stc.tstep  # in hertz
 stc_dur = stc.times[-1] - stc.times[0]
-del stc
 
 for freq in freqs:
     for prefix in (precamp_fname, postcamp_fname, median_split_fname,
@@ -56,38 +55,56 @@ for freq in freqs:
         # function:
         clu = (cluster_dict['tvals'], cluster_dict['clusters'],
                cluster_dict['pvals'], cluster_dict['hzero'])
-        # convert into a quasi-STC object where the first timepoint shows
-        # all clusters, each subsequent time point shows a single cluster,
-        # and the colormap indicates the duration for which the cluster was
-        # significant
-        has_signif_clusters = False
-        try:
-            cluster_stc = mne.stats.summarize_clusters_stc(
-                clu, vertices=vertices, tstep=stc_tstep_hz)
-            has_signif_clusters = True
-        except RuntimeError:
-            txt_fname = fname.replace('.npz', '_NO-SIGNIFICANT-CLUSTERS.txt')
-            txt_fpath = os.path.join(img_dir, txt_fname)
-            with open(txt_fpath, 'w') as f:
-                f.write('no significant clusters')
+        # since this is TFCE clustering, don't bother with
+        # mne.stats.summarize_clusters_stc, just stick the p-values into the
+        # existing STC.
+        stc.data = 1 - cluster_dict['pvals']
+        lims = (0.95, 0.99, 0.999)
+        clim_dict = dict(kind='value', lims=lims)
+        # save the STC
+        stc.save(os.path.join(stc_dir, fname.replace('.npz', '')))
+        brain = stc.plot(smoothing_steps='nearest',
+                         clim=clim_dict,
+                         time_unit='s',
+                         time_label=f'{freq} Hz',
+                         initial_time=0,
+                         **brain_plot_kwargs)
+        img_fname = re.sub(r'\.npz$', '.png', fname)
+        img_path = os.path.join(img_dir, img_fname)
+        brain.save_image(img_path)
 
-        if has_signif_clusters:
-            # save the quasi-STC
-            cluster_stc.save(os.path.join(stc_dir, fname.replace('.npz', '')))
-            # get indices for which clusters are significant
-            signif_clu = cluster_dict['good_cluster_idxs'][0]
-            # plot the clusters (saves as PNG image)
-            clim_dict = dict(kind='value', pos_lims=[0, stc_tstep_hz, stc_dur])
-            for time_idx, this_time in enumerate(cluster_stc.times):
-                if time_idx == 0:
-                    continue
-                brain = cluster_stc.plot(smoothing_steps='nearest',
-                                         clim=clim_dict,
-                                         time_unit='s',
-                                         time_label='frequency (Hz)',
-                                         initial_time=this_time,
-                                         **brain_plot_kwargs)
-                cluster_idx = signif_clu[time_idx - 1]
-                img_fname = re.sub(r's\.npz$', f'_{cluster_idx:05}.png', fname)
-                img_path = os.path.join(img_dir, img_fname)
-                brain.save_image(img_path)
+        ## convert into a quasi-STC object where the first timepoint shows
+        ## all clusters, each subsequent time point shows a single cluster,
+        ## and the colormap indicates the duration for which the cluster was
+        ## significant
+        #has_signif_clusters = False
+        #try:
+        #    cluster_stc = mne.stats.summarize_clusters_stc(
+        #        clu, vertices=vertices, tstep=stc_tstep_hz)
+        #    has_signif_clusters = True
+        #except RuntimeError:
+        #    txt_fname = fname.replace('.npz', '_NO-SIGNIFICANT-CLUSTERS.txt')
+        #    txt_fpath = os.path.join(img_dir, txt_fname)
+        #    with open(txt_fpath, 'w') as f:
+        #        f.write('no significant clusters')
+        #
+        #if has_signif_clusters:
+        #    # save the quasi-STC
+        #    cluster_stc.save(os.path.join(stc_dir, fname.replace('.npz', '')))
+        #    # get indices for which clusters are significant
+        #    signif_clu = cluster_dict['good_cluster_idxs'][0]
+        #    # plot the clusters (saves as PNG image)
+        #    clim_dict = dict(kind='value', pos_lims=[0, stc_tstep_hz, stc_dur])
+        #    for time_idx, this_time in enumerate(cluster_stc.times):
+        #        if time_idx == 0:
+        #            continue
+        #        brain = cluster_stc.plot(smoothing_steps='nearest',
+        #                                 clim=clim_dict,
+        #                                 time_unit='s',
+        #                                 time_label='frequency (Hz)',
+        #                                 initial_time=this_time,
+        #                                 **brain_plot_kwargs)
+        #        cluster_idx = signif_clu[time_idx - 1]
+        #        img_fname = re.sub(r's\.npz$', f'_{cluster_idx:05}.png', fname)
+        #        img_path = os.path.join(img_dir, img_fname)
+        #        brain.save_image(img_path)
