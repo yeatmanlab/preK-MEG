e132ceeef7fa72b5428ee2be4bd5477887df791f
Daniel McCloy <dan@mccloy.info>
2020-05-29 10:58:33 -0700

    finer steps

--- analysis/erp/ssvep_threshold_tvals.py
+++ analysis/erp/ssvep_threshold_tvals.py
@@ -57,7 +57,7 @@ for prefix in (precamp_fname,):
     brain = stc.plot(smoothing_steps='nearest', clim=clim, time_unit='s',
                      time_label='t-value (%0.2f Hz)', initial_time=freq,
                      **brain_plot_kwargs)
-    for threshold in range(3, 8):
+    for threshold in np.arange(4, 6.6, 0.5):
         for hemi in ('lh', 'rh'):
             tv = tvals[:hemi_n_verts] if hemi == 'lh' else tvals[hemi_n_verts:]
             verts = np.where(tv[:, bin_idx] >= threshold)[0]
@@ -65,7 +65,7 @@ for prefix in (precamp_fname,):
             # fill in verts that are surrounded by cluster verts
             label = label.fill(fsaverage_src)
             brain.add_label(label, borders=True, color='m')
-        img_fname = f'{prefix}-{freq:02}_Hz-threshold_{threshold:02}.png'
+        img_fname = f'{prefix}-{freq:02}_Hz-threshold_{threshold:3.1}.png'
         img_path = os.path.join(fig_dir, img_fname)
         brain.save_image(img_path)
         brain.remove_labels()
