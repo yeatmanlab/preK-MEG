6078acd4da7558e4b8759ea22f87fc557c2dd4ab
Daniel McCloy <dan@mccloy.info>
2020-05-29 10:11:58 -0700

    add thresholding script

---
+++ analysis/erp/ssvep_threshold_tvals.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Daniel McCloy
+
+Plot uncorrected t-value maps (of 2 Hz pre_camp) at various threshold levels.
+"""
+
+import os
+import numpy as np
+from mayavi import mlab
+import mne
+from aux_functions import load_paths, load_params
+
+mlab.options.offscreen = True
+mne.cuda.init_cuda()
+
+# flags
+save_movie = True
+
+# config paths
+data_root, subjects_dir, results_dir = load_paths()
+stc_dir = os.path.join(results_dir, 'pskt', 'group-level', 'stc')
+tval_dir = os.path.join(results_dir, 'pskt', 'group-level', 'tvals')
+fig_dir = os.path.join(results_dir, 'pskt', 'group-level', 'fig', 'thresholds')
+os.makedirs(fig_dir, exist_ok=True)
+
+# load params
+brain_plot_kwargs, movie_kwargs, subjects = load_params()
+
+# load an STC as a template
+fname = 'GrandAvg-pre_camp-pskt-5_sec-fft-avg'
+stc = mne.read_source_estimate(os.path.join(stc_dir, fname))
+all_freqs = stc.times
+vertices = stc.vertices
+
+# config other
+timepoints = ('pre',)
+precamp_fname = 'GrandAvg-pre_camp'
+freq = 2
+bin_idx = np.argmin(np.abs(all_freqs - freq))
+
+for prefix in (precamp_fname,):
+    fname = f'{prefix}-tvals.npy'
+    tvals = np.load(os.path.join(tval_dir, fname))
+    stc.data = tvals
+    # set the colormap lims
+    lims = tuple(np.percentile(tvals, (95, 99, 99.9)))
+    clim = dict(kind='value', lims=lims)
+    # plot the brain
+    brain = stc.plot(smoothing_steps='nearest', clim=clim, time_unit='s',
+                     time_label='t-value (%0.2f Hz)', initial_time=freq,
+                     **brain_plot_kwargs)
+    for threshold in range(3, 8):
+        verts = vertices[np.where(tvals[:, bin_idx] >= threshold)]
+        label = mne.Label(verts, hemi='both', subject=stc.subject)
+        brain.add_label(label, borders=True, color='m')
+        img_fname = f'{prefix}-{freq:02}_Hz-threshold_{threshold:02}.png'
+        img_path = os.path.join(fig_dir, img_fname)
+        brain.save_image(img_path)
+        brain.remove_labels()
+    del brain
