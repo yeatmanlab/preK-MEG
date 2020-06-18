487bebc5a7b97b012341e8b4fba631ab861d2997
Daniel McCloy <dan@mccloy.info>
2020-06-17 17:00:25 -0700

    move files & make relative imports work

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sswef',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/yeatman-lab/SSWEF',
+      author='Daniel McCloy',
+      author_email='dan@mccloy.info',
+      license='MIT',
+      packages=['analysis'],
+      zip_safe=False)
