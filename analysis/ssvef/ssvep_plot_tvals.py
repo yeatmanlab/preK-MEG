bbd1104526933aed7fc8ce8d15025de0a9fc3c55
Daniel McCloy <dan@mccloy.info>
2020-06-24 16:13:26 -0700

    fix fname

--- analysis/ssvef/ssvep_plot_tvals.py
+++ analysis/ssvef/ssvep_plot_tvals.py
@@ -45,7 +45,7 @@ median_split_fname = 'UpperVsLowerKnowledge-pre_camp'
 intervention_fname = 'LetterVsLanguageIntervention-PostMinusPre_camp'
 
 # load an STC as a template
-fname = 'GrandAvg-pre_camp-pskt-5_sec-fft-avg'
+fname = 'GrandAvg-pre_camp-pskt-5_sec-fft-amp'
 stc = mne.read_source_estimate(os.path.join(stc_dir, fname))
 
 for prefix in (precamp_fname, postcamp_fname, median_split_fname,
