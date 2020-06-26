acfcb702b2500950d6244a62980ae4cf5274983f
Daniel McCloy <dan@mccloy.info>
2020-06-26 14:35:00 -0700

    add notebook runner bash script

---
+++ analysis/notebooks/render_ROI_modeling_notebook_on_colchuck.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+conda activate rdev
+echo "This will take a LONG TIME, will CHANGE THE ACTIVE CONDA ENVIRONMENT, and will OVERWRITE the input file!!!"
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=ir --ExecutePreprocessor.timeout=-1 --execute ssvep_ROI_modeling.ipynb --inplace
