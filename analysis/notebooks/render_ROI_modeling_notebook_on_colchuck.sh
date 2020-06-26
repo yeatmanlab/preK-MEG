b4e2a8d297ba14b43b52f0b12fb6fb4140de667a
Daniel McCloy <dan@mccloy.info>
2020-06-26 14:40:52 -0700

    split lines; verbose

--- analysis/notebooks/render_ROI_modeling_notebook_on_colchuck.sh
+++ analysis/notebooks/render_ROI_modeling_notebook_on_colchuck.sh
@@ -2,4 +2,9 @@
 
 conda activate rdev
 echo "This will take a LONG TIME, will CHANGE THE ACTIVE CONDA ENVIRONMENT, and will OVERWRITE the input file!!!"
-jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=ir --ExecutePreprocessor.timeout=-1 --execute ssvep_ROI_modeling.ipynb --inplace
+jupyter nbconvert --to notebook \
+    --ExecutePreprocessor.kernel_name=ir \
+    --ExecutePreprocessor.timeout=-1 \
+    --log-level INFO \
+    --inplace \
+    --execute ssvep_ROI_modeling.ipynb
