2e4753f0dbbe6bf43b2f61dd702cc25afc944b41
Daniel McCloy <dan@mccloy.info>
2020-09-12 17:22:26 -0500

    missed one

--- analysis/ssvef/prek_mnefun_ssvep.py
+++ analysis/ssvef/prek_mnefun_ssvep.py
@@ -14,54 +14,42 @@ Notes:
 
 """
 
-import os
-import os.path as op
-import mnefun
 import numpy as np
-import yaml
+import mnefun
+from analysis.aux_functions import load_params, load_paths
 
 print(mnefun)
 
-fixed_or_twa = 'twa' # convenience variable for doing different runs
+fixed_or_twa = 'twa'  # convenience variable for doing different runs
 pre_or_post = 'post'
-r_cohort = True
 
 if fixed_or_twa == 'fixed':
     trans_to = (0., 0., 0.04)
 else:
     trans_to = 'twa'
 
-pskt_dir = '/mnt/scratch/prek/%s_camp/%s_hp/pskt/' % (pre_or_post, fixed_or_twa)
+_, subjects_dir, _ = load_paths()
 
 # load subjects
-if r_cohort:
-   pskt_dir = '/mnt/scratch/prek/r_cohort/%s_camp/%s_hp/pskt/' % (pre_or_post, fixed_or_twa) 
-else:
-   pskt_dir = '/mnt/scratch/prek/%s_camp/%s_hp/pskt/' % (pre_or_post, fixed_or_twa)
-   
-if r_cohort:
-    with open(os.path.join('..', '..', 'params', 'r_cohort_subjects.yaml'), 'r') as f:
-        subjects = yaml.load(f, Loader=yaml.FullLoader)
-else:
-    with open(os.path.join('..', '..', 'params', 'subjects.yaml'), 'r') as f:
-        subjects = yaml.load(f, Loader=yaml.FullLoader)
-        
-subjects.sort()
-subjects.remove('prek_2171')
-subjects.remove('prek_2259')
+*_, subjects, cohort = load_params()
 
-structurals = subjects if r_cohort else [x.upper() for x in subjects]
+if cohort == 'replication':
+    pskt_dir = f'/mnt/scratch/prek/r_cohort/{pre_or_post}_camp/{fixed_or_twa}_hp/pskt/'  # noqa E501
+else:
+    pskt_dir = f'/mnt/scratch/prek/{pre_or_post}_camp/{fixed_or_twa}_hp/pskt/'
+
+structurals = [x.upper() for x in subjects]
 
 params = mnefun.Params(tmin=-0.1, tmax=1, n_jobs='cuda',
                        proj_sfreq=200, n_jobs_fir='cuda',
-                       filter_length='5s', lp_cut=80., 
+                       filter_length='5s', lp_cut=80.,
                        n_jobs_resample='cuda',
                        bmin=-0.1, bem_type='5120', )
 
 params.subjects = subjects
 params.work_dir = pskt_dir
 params.structurals = structurals
-params.subjects_dir = '/mnt/scratch/prek/anat/r_cohort_anat' if r_cohort else '/mnt/scratch/prek/anat'
+params.subjects_dir = subjects_dir
 params.dates = [(2013, 0, 00)] * len(params.subjects)
 # define which subjects to run
 params.subject_indices = np.arange(len(params.subjects))
@@ -75,12 +63,12 @@ params.sws_dir = '/data07/nordme/prek/'
 # SSS options
 params.sss_type = 'python'
 params.sss_regularize = 'in'
-params.tsss_dur = 4. # tSSS duration
+params.tsss_dur = 4.  # tSSS duration
 params.int_order = 8
 params.st_correlation = .98
-params.trans_to = trans_to # time weighted average or fixed head position (change this to fixed pos for sensor analysis)
+params.trans_to = trans_to  # "twa" (time-weighted avg) or "fixed" head pos; use "fixed" for within-subj sensor-level analyses
 params.coil_t_window = 'auto'
-params.movecomp='inter'
+params.movecomp = 'inter'
 # remove segments with < 3 good coils for at least 100 ms
 params.coil_bad_count_duration_limit = 0.1
 # Trial rejection criteria
@@ -109,21 +97,21 @@ params.cov_rank = None
 # No conditions for report
 
 params.report_params.update(  # add plots
-    bem=True, 
+    bem=True,
     psd=False
-)   
+)
 
 mnefun.do_processing(
     params,
     fetch_raw=False,
-    do_sss=False, # do tSSS
-    do_score=False,  # do scoring
-    gen_ssp=True, # generate ssps
-    apply_ssp=True, # apply ssps
-    write_epochs=False, # epoching & filtering
-    gen_covs=False, # make covariance 
-    gen_fwd=False, # generate fwd model
-    gen_inv=False, # general inverse
-    gen_report=True, #print report
-    print_status=True # show status
+    do_sss=False,        # do tSSS
+    do_score=False,      # do scoring
+    gen_ssp=True,        # generate ssps
+    apply_ssp=True,      # apply ssps
+    write_epochs=False,  # epoching & filtering
+    gen_covs=False,      # make covariance
+    gen_fwd=False,       # generate fwd model
+    gen_inv=False,       # general inverse
+    gen_report=True,     # print report
+    print_status=True    # show status
 )
