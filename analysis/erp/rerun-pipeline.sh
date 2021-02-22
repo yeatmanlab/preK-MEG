13d7b4458035d8154e736168b72745d58358078f
Daniel McCloy <dan.mccloy@gmail.com>
2021-02-22 12:52:11 -0800

    update bash pipeline script

--- analysis/erp/rerun-pipeline.sh
+++ analysis/erp/rerun-pipeline.sh
@@ -4,8 +4,8 @@ export DISPLAY=:1.0
 python prek_make_stcs.py && \
 python prek_make_group_averages.py && \
 python prek_do_contrasts.py && \
-python prek_extract_ROI_time_courses.py #&& \
-#python prek_clustering.py && \
-#python prek_plot_clusters_quick_and_dirty.py && \
+python prek_extract_ROI_time_courses.py && \
+python prek_clustering.py && \
+python prek_plot_clusters_quick_and_dirty.py #&& \
 #python prek_plot_clusters.py && \
 #bash prek_make_cluster_movie_from_stills.bash
