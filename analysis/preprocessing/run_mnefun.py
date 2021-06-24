6200a8e548ccf1f8693550bd6c3eb1a75fb619ff
Daniel McCloy <dan.mccloy@gmail.com>
2021-06-24 11:14:37 -0700

    return to separate preprocessing (but keep new blink annots)

--- analysis/preprocessing/run_mnefun.py
+++ analysis/preprocessing/run_mnefun.py
@@ -28,39 +28,48 @@ params.dates = [(2013, 0, 00)] * len(params.subjects)
 # loop over pre/post intervention recordings, and over head pos transforms
 for prepost in ('pre', 'post'):
     for headpos in ('twa', 'fixed'):
-        # set variable-contingent params
-        params.work_dir = os.path.join(data_root, f'{prepost}_camp',
-                                       f'{headpos}_hp', 'combined')
-        params.trans_to = (0., 0., 0.04) if headpos == 'fixed' else headpos
-        # run filenames
-        erp_run_names = [f'%s_erp_{prepost}']
-        pskt_run_names = [f'%s_pskt_{run:02}_{prepost}' for run in (1, 2)]
-        params.run_names = erp_run_names + pskt_run_names
+        for experiment in ('pskt', 'erp'):  # XXX or ('combined',)
+            # set variable-contingent params
+            params.work_dir = os.path.join(data_root, f'{prepost}_camp',
+                                           f'{headpos}_hp', experiment)
+            params.trans_to = (0., 0., 0.04) if headpos == 'fixed' else headpos
+            # run filenames
+            erp_run_names = [f'%s_erp_{prepost}']
+            pskt_run_names = [f'%s_pskt_{run:02}_{prepost}' for run in (1, 2)]
+            if experiment == 'combined':
+                params.run_names = (erp_run_names + pskt_run_names)
+            elif experiment == 'pskt':
+                params.run_names = pskt_run_names
+            else:
+                params.run_names = erp_run_names
 
-        # set additional params: report
-        common_kw = dict(analysis='Conditions')
-        sns_kw = dict(times='peaks', **common_kw)
-        snr_kw = dict(inv=f'%s-{params.lp_cut}-sss-meg-inv.fif', **common_kw)
-        src_kw = dict(views=['lateral', 'ventral'], size=(800, 800), **snr_kw)
-        wht_kw = dict(cov=f'%s-{params.lp_cut}-sss-cov.fif', **common_kw)
-        conds = params.in_names  # experimental conditions
-        params.report['snr'] = [dict(name=c, **snr_kw) for c in conds]
-        params.report['sensor'] = [dict(name=c, **sns_kw) for c in conds]
-        params.report['source'] = [dict(name=c, **src_kw) for c in conds]
-        params.report['whitening'] = [dict(name=c, **wht_kw) for c in conds]
+            # set additional params: report
+            common_kw = dict(analysis='Conditions')
+            sns_kw = dict(times='peaks', **common_kw)
+            snr_kw = dict(inv=f'%s-{params.lp_cut}-sss-meg-inv.fif',
+                          **common_kw)
+            src_kw = dict(views=['lateral', 'ventral'], size=(800, 800),
+                          **snr_kw)
+            wht_kw = dict(cov=f'%s-{params.lp_cut}-sss-cov.fif', **common_kw)
+            conds = params.in_names  # experimental conditions
+            params.report['snr'] = [dict(name=c, **snr_kw) for c in conds]
+            params.report['sensor'] = [dict(name=c, **sns_kw) for c in conds]
+            params.report['source'] = [dict(name=c, **src_kw) for c in conds]
+            params.report['whitening'] = [dict(name=c, **wht_kw)
+                                          for c in conds]
 
-        # run it
-        mnefun.do_processing(
-            params,
-            fetch_raw=False,      # go get the Raw files
-            do_sss=True,          # tSSS / maxwell filtering
-            do_score=True,        # run scoring function to extract events
-            gen_ssp=True,         # create SSP projectors
-            apply_ssp=True,       # apply SSP projectors
-            write_epochs=True,    # epoching & filtering
-            gen_covs=True,        # make covariance
-            gen_fwd=True,         # generate fwd model
-            gen_inv=True,         # generate inverse
-            gen_report=True,      # print report
-            print_status=True     # show status
-        )
+            # run it
+            mnefun.do_processing(
+                params,
+                fetch_raw=False,      # go get the Raw files
+                do_sss=True,          # tSSS / maxwell filtering
+                do_score=True,        # run scoring function to extract events
+                gen_ssp=True,         # create SSP projectors
+                apply_ssp=True,       # apply SSP projectors
+                write_epochs=True,    # epoching & filtering
+                gen_covs=True,        # make covariance
+                gen_fwd=True,         # generate fwd model
+                gen_inv=True,         # generate inverse
+                gen_report=True,      # print report
+                print_status=True     # show status
+            )
