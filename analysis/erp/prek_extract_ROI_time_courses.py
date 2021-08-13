7cb37fa43edd57db3cb04c8e1ad3e6e4ce57626e
Daniel McCloy <dan.mccloy@gmail.com>
2021-08-13 13:18:46 -0700

    include new labels

--- analysis/erp/prek_extract_ROI_time_courses.py
+++ analysis/erp/prek_extract_ROI_time_courses.py
@@ -69,7 +69,9 @@ for freq in (4,):
             label.color = to_rgba(roi_colors[ix % len(roi_colors)])
             rois[slug] = label
 # add custom labels
-fnames = ('2Hz_LetterKnowledge.lh.label', 'MPM_IOS_IOG_lh.label', 'MPM_pOTS_lh.label')
+fnames = ('2Hz_LetterKnowledge.lh.label',
+          'MPM_IOS_IOG_lh.label',
+          'MPM_pOTS_lh.label')
 for fname in fnames:
     fpath = os.path.join(roi_dir, fname)
     label = mne.read_label(fpath)
