db134926cf7c8e5981980ccef3ab7fcf3e856aea
Daniel McCloy <dan.mccloy@gmail.com>
2021-08-25 13:26:17 -0700

    update intxcondxtime figure

--- analysis/final-figs/erp-lineplot-grid-intervention-by-condition-by-timepoint.py
+++ analysis/final-figs/erp-lineplot-grid-intervention-by-condition-by-timepoint.py
@@ -9,12 +9,11 @@ Plot FFT and SNR spectra for a given label, for various splits of the data.
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.image import imread
 from matplotlib.lines import Line2D
 import pandas as pd
 import seaborn as sns
 
-from analysis.aux_functions import load_paths
+from analysis.aux_functions import load_paths, yamload
 
 
 def nice_ticklabels(ticks):
@@ -26,11 +25,15 @@ data_root, subjects_dir, results_dir = load_paths()
 timeseries_dir = os.path.join(results_dir, 'roi', 'time-series')
 
 # load data
+method = 'dSPM'  # ('dSPM', 'MNE')
 region = 'MPM_IOS_IOG_pOTS_lh'
 fname = f'roi-{region}-timeseries-long.csv'
 df = pd.read_csv(os.path.join(timeseries_dir, fname), index_col=0)
-peak_of_grand_mean = 0.185
-temporal_roi = np.array([-0.05, 0.05]) + peak_of_grand_mean
+df = df.query(f'method=="{method}"')
+# load temporal ROI
+with open('peak-of-grand-mean.yaml', 'r') as f:
+    peak_properties = yamload(f)
+temporal_roi = np.array(peak_properties['temporal_roi'])
 
 # config plot
 sns.set_style('white')
@@ -46,7 +49,6 @@ colors = dict(words=('#EE99AA',   # light red
 interventions = ('letter', 'language')
 conditions = ('words', 'faces', 'cars')
 timepoints = ('pre', 'post')
-method = 'dSPM'  # ('dSPM', 'MNE')
 ymax = dict(MNE=1e-10, dSPM=4, sLORETA=2, fft=300, snr=8)[method]
 yticks = np.linspace(0, ymax, num=5)
 xticks = np.linspace(0, 1, num=5)
@@ -54,16 +56,7 @@ yticklabels = nice_ticklabels(yticks)
 xticklabels = nice_ticklabels(xticks)
 
 # init figure
-fig = plt.figure(figsize=(6.5, 4))
-brainfig, linefig = fig.subfigures(1, 2, width_ratios=(1, 3), wspace=0.1)
-
-# add ROI
-img_path = os.path.join('.', 'MPM_IOS_IOG_pOTS_lh.png')
-brainax = brainfig.subplots()
-brain_image = imread(img_path)
-brainax.imshow(brain_image, interpolation=None)
-brainax.set_axis_off()
-brainfig.subplots_adjust(bottom=0.3, top=0.95)
+linefig = plt.figure(figsize=(6.5, 4))
 
 # do lineplots
 axs = linefig.subplots(2, 3, sharex=True, sharey=True)
@@ -71,8 +64,7 @@ for this_intervention, row in zip(interventions, axs):
     for this_condition, ax in zip(conditions, row):
         spec = ax.get_subplotspec()
         this_df = df[(df['condition'] == this_condition) &
-                     (df['intervention'] == this_intervention) &
-                     (df['method'] == method)]
+                     (df['intervention'] == this_intervention)]
         with sns.color_palette(colors[this_condition]):
             sns.lineplot(x='time', y='value', ci=68, hue='timepoint',
                          style='timepoint', hue_order=timepoints,
@@ -80,12 +72,13 @@ for this_intervention, row in zip(interventions, axs):
                          legend=False, linewidth=1,
                          err_kws=dict(alpha=0.25, edgecolor='none'))
         # fill temporal ROI
-        ax.axvspan(*temporal_roi, color='0.9', zorder=-2)
+        ax.axvspan(*temporal_roi, facecolor='k', alpha=0.1, zorder=-2,
+                   edgecolor='none')
         # garnish
         ax.set(xlabel='', ylabel='', xlim=(-0.1, 1), ylim=(0, ymax),
                yticks=yticks, yticklabels=yticklabels,
                xticks=xticks, xticklabels=xticklabels)
-        ax.grid(color='0.9')
+        ax.grid(color='k', alpha=0.1)
         for spine in ax.spines.values():
             spine.set_edgecolor('0.5')
         # titles
@@ -99,20 +92,16 @@ for this_intervention, row in zip(interventions, axs):
 
 linefig.supxlabel('time (s)')
 linefig.supylabel('dSPM value')
-linefig.subplots_adjust(left=0.1, bottom=0.12, right=0.97, top=0.93,
+linefig.subplots_adjust(left=0.1, bottom=0.12, right=0.97, top=0.9,
                         hspace=0.3, wspace=0.1)
 
 legend_lines = (Line2D([], [], color='0.6', label='pre-intervention',
                        linestyle='--', linewidth=1),
                 Line2D([], [], color='k', label='post-intervention',
                        linewidth=1))
-axs[1, 0].legend(handles=legend_lines, ncol=1, loc='lower right',
-                 frameon=True, bbox_to_anchor=(-0.18, -0.06))
+axs[0, -1].legend(handles=legend_lines, ncol=2, loc='lower right',
+                  frameon=True, bbox_to_anchor=(1.04, 1.02))
 
-# add subfigure labels
-kwargs = dict(weight='bold', size='large', ha='left', va='top')
-brainfig.text(0.05, 0.98, 'A', **kwargs)
-linefig.text(0.015, 0.98, 'B', **kwargs)
-
-fig.savefig('lineplot-grid.png')
-fig.savefig('lineplot-grid.pdf', dpi=400)
+# save
+linefig.savefig('lineplot-grid.png')
+linefig.savefig('lineplot-grid.pdf', dpi=400)
