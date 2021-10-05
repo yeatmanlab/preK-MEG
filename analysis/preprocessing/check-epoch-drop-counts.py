59f9c84fd4c3ad78d7566a0eba50658f27829db8
Daniel McCloy <dan.mccloy@gmail.com>
2021-10-05 12:57:27 -0700

    make many diagnostic plots of epoch rejection

--- analysis/preprocessing/check-epoch-drop-counts.py
+++ analysis/preprocessing/check-epoch-drop-counts.py
@@ -8,8 +8,10 @@ compute and plot epoch peak-to-peak histograms and epochs retained per
 condition.
 """
 
+import os
 import yaml
 from collections import Counter
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import mne
@@ -72,12 +74,65 @@ for tpt in ('pre', 'post'):
         row = pd.DataFrame(dict(subj=[subj], timepoint=[tpt], **record))
         trial_count_df = pd.concat((trial_count_df, row))
 
+# add in subject-level R-values
+fname = (f'pre-post-correlations-mag{int(thresholds["mag"] * 1e15)}fT'
+         f'-grad{int(thresholds["grad"] * 1e13)}fTcm.csv')
+fpath = os.path.join('csv', fname)
+subj_crossval_df = pd.read_csv(fpath, index_col=0)
+rval_df = subj_crossval_df.pivot(
+    index=['subj', 'cond'], columns='hemi', values='rval'
+    ).rename(columns=lambda x: f'rval_{x}')
+epoch_df = trial_count_df.melt(
+    id_vars=['subj', 'timepoint'],
+    value_vars=['words', 'faces', 'cars', 'aliens'],
+    var_name='cond', value_name='n_epochs'
+    ).pivot(
+        index=['subj', 'cond'], columns='timepoint', values='n_epochs'
+    ).rename(columns=lambda x: f'n_epochs_{x}')
+merged_df = rval_df.merge(
+    epoch_df, left_index=True, right_index=True).reset_index()
 
+# plot histogram of peak-to-peak epoch amplitudes
 g = sns.FacetGrid(peak_to_peak_df, row='ch_type', sharex=False)
 g.map(sns.histplot, 'ptp_ampl', stat='count')
 for ch_type, ax in g.axes_dict.items():
     ax.axvline(x=thresholds[ch_type], color='C1', ls='--')
 g.fig.savefig('peak-to-peak-hists-and-rejection-thresholds.png')
 
+# scatterplot r-values in left and right hemispheres
+fig, ax = plt.subplots()
+sns.scatterplot(x='rval_lh', y='rval_rh', hue='cond', style='cond',
+                data=merged_df, legend=False, ax=ax)
+ax.grid(True)
+fig.savefig('rval-scatterplot.png')
+
+# dotplot of r-values for each subj
+g = sns.PairGrid(data=merged_df.sort_values(['rval_lh', 'rval_rh']),
+                 x_vars=['rval_lh', 'rval_rh'],
+                 y_vars=['subj'],
+                 hue='cond', height=10, aspect=0.3)
+g.map(sns.stripplot, orient='h', jitter=False, linewidth=1, edgecolor='w')
+for ax in g.axes.flat:
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(True)
+    ax.axvline(0, color='0.5', linewidth=1)
+g.set(xlim=(-1, 1), ylabel="")
+g.fig.subplots_adjust(left=0.2, wspace=0.4)
+sns.despine(left=True, bottom=True)
+g.fig.savefig('rval-dotplot.png')
+
+# distribution of r-values across subjects
+melted = merged_df.melt(id_vars=['subj'],
+                        value_vars=['rval_lh', 'rval_rh'],
+                        value_name='rval')
+rval_distr = melted.groupby('subj').agg('mean').sort_values('rval')
+ax = sns.histplot(rval_distr, x='rval', bins=int(round(len(subjects) // 3)))
+ax.set_title('distribution of mean R-values across subjects')
+ax.figure.savefig('rval-distribution.png')
+
+# save
+peak_to_peak_df.to_csv('unthresholded-epoch-peak-to-peak-amplitudes.csv',
+                       index=False)
 trial_count_df.to_csv('trial-counts-after-thresholding.csv', index=False)
+merged_df.to_csv('trial-counts-and-r-values.csv', index=False)
 # print(trial_count_df.query('cars<15 or words<15 or faces<15'))
