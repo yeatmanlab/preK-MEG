1f9c91869e9a188149c29f757f3d2ed7bc48a980
Daniel McCloy <dan.mccloy@gmail.com>
2021-12-20 11:26:19 -0800

    add/update bash rerunners

---
+++ analysis/final-figs/rerun.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export DISPLAY=:1.0
+
+python plot-ROI-label.py && \
+python erp-spatial-and-temporal-ROIs.py && \
+python erp-postcamp-lineplot-by-intervention-and-condition.py && \
+python erp-lineplot-grid-intervention-by-condition-by-timepoint.py && \
+python erp-rejection-thresholds.py
