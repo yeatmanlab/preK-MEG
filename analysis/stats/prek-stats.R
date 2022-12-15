cea1ec7291c77743e6bcc1ad0e2b7cc46f0ac1f8
Daniel McCloy <dan@mccloy.info>
2022-12-15 17:59:33 -0600

    add sanity check code and figure

--- analysis/stats/prek-stats.R
+++ analysis/stats/prek-stats.R
@@ -113,30 +113,85 @@ matrix(c(0, 1, 0, 0, 0, 1), nrow=3, ncol=2, byrow=FALSE,
        dimnames=list(c("words", "faces", "cars"), c("faces", "cars"))) ->
     contrasts(modeldata$cond_)
 
-# actually run the models
-formula(value ~ cond_ * tmpt_ * intv_ + (1 + cond_ + tmpt_ | subj)) -> form
+
+# sanity check: if we run a regular (non-mixed) lm on pre-subtracted data
+# (post-pre and words-cars) do we get what we expect?
 modeldata %>%
+    tidyr::pivot_wider(id_cols=c(time, subj, cond_, intv_), names_from=tmpt_,
+                       values_from=value) %>%
+    mutate(post_minus_pre=post - pre) %>%
+    select(-pre, -post) %>%
+    tidyr::pivot_wider(id_cols=c(time, subj, intv_), names_from=cond_,
+                       values_from=post_minus_pre) %>%
+    mutate(words_minus_faces=words - faces,
+           words_minus_cars=words - cars) %>%
+    select(-words, -faces, -cars) ->
+    sanitycheckdata
+
+formula(words_minus_faces ~ intv_) -> form1
+formula(words_minus_cars ~ intv_) -> form2
+sanitycheckdata %>%
     nest_by(time) %>%
-    mutate(model=list(afex::lmer_alt(formula=form, data=data, method="S",
-                                     check_contrasts=FALSE))) %>%
-    summarise(broom.mixed::tidy(model)) %>%
-    tidyr::unnest(cols=c()) ->
-    mod
-readr::write_csv(mod, file="full-model-results.csv")
+    mutate(words_minus_faces=list(lm(formula=form1, data=data)),
+           words_minus_cars=list(lm(formula=form2, data=data))) %>%
+    tidyr::pivot_longer(cols=c(words_minus_faces, words_minus_cars),
+                        names_to="condition",
+                        values_to="model") %>%
+    rowwise() %>%
+    summarise(time, condition, broom:::tidy.lm(model)) %>%
+    arrange(condition) ->
+    sanitycheckmod
 
-# reduce the dataframe to just coef and p-value of fixed effects
-mod %>%
-    filter(effect == "fixed") %>%
-    select(time, term, estimate, statistic, p.value) %>%
-    rename(t=statistic, p=p.value, coef=estimate) %>%
-    mutate(neglogp=-log10(p)) ->
-    mod_short
-readr::write_csv(mod_short, file="short-model-results.csv")
+sanitycheckmod %>%
+    mutate(neglogp=-log10(p.value)) %>%
+    filter(!term %in% "(Intercept)") %>%
+    ggplot(mapping=aes(x=time, y=neglogp)) +
+    facet_wrap(vars(condition)) +
+    geom_vline(xintercept=0, col="gray60") +
+    geom_line() +
+    labs(y="-log₁₀(p)", x="time (s)") ->
+    gg
 
-# run one model at t=0 in case we need to inspect the model object
-afex::lmer_alt(formula=form, data=filter(modeldata, time == 0.0), method="S",
-               check_contrasts=FALSE) ->
-    one_model
+# add horizontal reference lines corresponding to interesting p-values
+list(red=0.05, blue=0.01, brown=0.001) -> pvals
+for (i in seq_along(pvals)) {
+    pvals[[i]] -> p
+    -log10(p) -> thresh
+    names(pvals)[i] -> color
+    gg +
+        geom_hline(yintercept=thresh, linetype="dotted", col=color) +
+        annotate("text", x=1, y=thresh, label=stringr::str_c("p=", p), hjust=1,
+                 vjust=-0.5, col=color, size=6/.pt) ->
+        gg
+
+}
+ggsave(filename="sanity-check-pvals.png", plot=gg)
+
+
+# # actually run the full mixed models
+# formula(value ~ cond_ * tmpt_ * intv_ + (1 + cond_ + tmpt_ | subj)) -> form
+# modeldata %>%
+#     nest_by(time) %>%
+#     mutate(model=list(afex::lmer_alt(formula=form, data=data, method="S",
+#                                      check_contrasts=FALSE))) %>%
+#     summarise(broom.mixed::tidy(model)) %>%
+#     tidyr::unnest(cols=c()) ->
+#     mod
+# readr::write_csv(mod, file="full-model-results.csv")
+#
+# # reduce the dataframe to just coef and p-value of fixed effects
+# mod %>%
+#     filter(effect == "fixed") %>%
+#     select(time, term, estimate, statistic, df, p.value) %>%
+#     rename(t=statistic, p=p.value, coef=estimate) %>%
+#     mutate(neglogp=-log10(p)) ->
+#     mod_short
+# readr::write_csv(mod_short, file="short-model-results.csv")
+#
+# # run one model at t=0 in case we need to inspect the model object
+# afex::lmer_alt(formula=form, data=filter(modeldata, time == 0.0), method="S",
+#                check_contrasts=FALSE) ->
+#     one_model
 
 
 # # # # # # #
